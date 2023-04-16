b1f8ac38e3ecb7993328fd45032724358e5aae86
Graham Jarvis <zeroThousand111@proton.me>
2023-04-16 13:52:41 +0000

    create odd.rb to solve exercise of the same name in the easy 1 set

---
+++ rb101/exercises_small_problems/easy1/odd.rb
@@ -0,0 +1,14 @@
+def is_odd?(integer)
+  if integer % 2 == 1
+    true
+  else
+    false
+  end
+end
+
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
