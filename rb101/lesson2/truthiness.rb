375c51bc0454ad79e8ae42b65fc4c764450706e6
Graham Jarvis <zeroThousand111@proton.me>
2023-04-16 10:07:39 +0000

    create truthiness.rb to test examples in lesson 2

---
+++ rb101/lesson2/truthiness.rb
@@ -0,0 +1,9 @@
+p true.class          # => TrueClass
+p true.nil?           # => false
+p true.to_s           # => "true"
+p true.methods        # => list of methods you can call on the true object
+
+p false.class         # => FalseClass
+p false.nil?          # => false
+p false.to_s          # => "false"
+p false.methods       # => list of methods you can call on the false object
