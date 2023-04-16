e8f65d23d2f1e351d388be6569eaa8a44253943c
Graham Jarvis <zeroThousand111@proton.me>
2023-04-16 14:16:13 +0000

    add official solution to odd.rb

--- rb101/exercises_small_problems/easy1/odd.rb
+++ rb101/exercises_small_problems/easy1/odd.rb
@@ -1,3 +1,4 @@
+# My Original Over-Engineered Solution
 # def is_odd?(integer)
 #   if integer % 2 == 1
 #     true
@@ -6,9 +7,22 @@
 #   end
 # end
 
-def is_odd?(integer)
-  (integer % 2 == 1) ? true : false
-end
+# A Ternary Statement that does the same as above
+# def is_odd?(integer)
+#   (integer % 2 == 1) ? true : false
+# end
+
+# Official Solution
+# def is_odd?(number)
+#   number % 2 == 1
+# end
+
+# Answer to the further exploration question
+# def is_odd?(integer)
+#   integer.remainder(2)
+# end
+
+
 
 puts is_odd?(2)    # => false
 puts is_odd?(5)    # => true
