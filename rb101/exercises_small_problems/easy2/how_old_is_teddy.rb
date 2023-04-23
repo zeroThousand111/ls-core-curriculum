93bef3c55a5e0039fe88f807896ec3de54dfcc59
Graham Jarvis <zeroThousand111@proton.me>
2023-04-23 13:16:07 +0000

    refactor how_old_is_teddy.rb to complete further exploration

--- rb101/exercises_small_problems/easy2/how_old_is_teddy.rb
+++ rb101/exercises_small_problems/easy2/how_old_is_teddy.rb
@@ -1,5 +1,12 @@
+# ask user for name input
+puts '=> What is the name of the person?'
+name = gets.chomp
+
+# if name is blank, change it to Teddy
+name = 'Teddy' if name == ''
+
 # generate Teddy's age
 age = rand(20..200)
 
 # output to user
-puts "Teddy is #{age} years old!"
+puts "#{name} is #{age} years old!"
