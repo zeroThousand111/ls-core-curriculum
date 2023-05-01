5d752fade591dc9d2a3271b8996a31e914d555c5
Graham Jarvis <zeroThousand111@proton.me>
2023-05-01 13:41:52 +0000

    refactor  mortgage_calculator.rb after code review feedback

--- rb101/lesson2/mortgage_calculator.rb
+++ rb101/lesson2/mortgage_calculator.rb
@@ -1,3 +1,6 @@
+# initialise constant
+MONTHS_IN_YEAR = 12
+
 # user input validation methods
 def valid_float?(n)
   n.to_f.to_s == n
@@ -15,11 +18,63 @@ def positive_number?(n)
   n.to_i > 0
 end
 
+def zero_or_above?(n)
+  n.to_i >= 0
+end
+
 def valid_loan_duration?(n)
   valid_number?(n) && positive_number?(n)
 end
 
-# welcome heredoc
+# calculation method
+def calculate_monthly_payment(loan_amount, loan_duration_months, monthly_interest_rate)
+  loan_amount * (monthly_interest_rate / (1 -
+                (1 + monthly_interest_rate)**(-loan_duration_months)))
+end
+
+# methods to obtain user input
+def get_loan_amount
+  loan_amount = nil
+  loop do
+    puts "What is the amount of the loan ($)?"
+    loan_amount = gets.chomp
+    break if valid_number?(loan_amount) && positive_number?(loan_amount)
+    puts "Invalid loan amount."
+    puts "Please only enter a number; no characters or symbols."
+    puts "Please don't enter a value of zero."
+    puts "Please don't enter any spaces or commas in long numbers."
+  end
+  loan_amount.to_f
+end
+
+def get_apr
+  apr = nil
+  loop do
+    puts "What is the Annual Percentage Rate on the loan (%)?"
+    apr = gets.chomp
+    break if valid_number?(apr) && zero_or_above?(apr)
+    puts "Invalid APR."
+    puts "Please only enter a number; no characters or symbols."
+    puts "Please don't enter any spaces or commas in long numbers."
+    puts "Please only enter a positive value or zero for the APR."
+  end
+  apr.to_f / 100
+end
+
+def get_loan_duration
+  loan_duration_years = nil
+  loop do
+    puts "What is the duration of the loan (years)?"
+    loan_duration_years = gets.chomp
+    break if valid_loan_duration?(loan_duration_years)
+    puts "Invalid duration."
+    puts "Please only enter a number; no characters or symbols."
+    puts "Please don't enter a value of zero."
+    puts "Please don't enter any spaces or commas in long numbers."
+  end
+  loan_duration_years
+end
+
 welcome = <<-WELCOME
 Welcome to Mortgage Calculator!
 
@@ -34,25 +89,6 @@ In a moment I will ask for:
 
 WELCOME
 
-# error heredocs
-error = <<-ERROR
-Please only enter a number; no characters or symbols.
-Please don't enter a value of zero.
-Please don't enter any spaces or commas in long numbers.
-
-ERROR
-
-error_apr = <<-ERROR
-Please only enter a number; no characters or symbols.
-Please don't enter any spaces or commas in long numbers.
-
-ERROR
-
-# initialise local variables that will be out of scope in later loops
-loan_amount = nil
-apr = nil
-loan_duration_years = nil
-
 # Main loop
 loop do
   # Welcome message
@@ -60,51 +96,24 @@ loop do
   puts welcome
 
   # get user input for three variables
-  # get loan amount
-  loop do
-    puts "What is the amount of the loan ($)?"
-    loan_amount = gets.chomp
-    break if valid_number?(loan_amount) && positive_number?(loan_amount)
-    puts "Invalid loan amount."
-    puts error
-  end
-
-  # get APR
-  loop do
-    puts "What is the Annual Percentage Rate on the loan (%)?"
-    apr = gets.chomp
-    break if valid_number?(apr)
-    puts "Invalid APR."
-    puts error_apr
-  end
-
-  # get loan duration
-  loop do
-    puts "What is the duration of the loan (years)?"
-    loan_duration_years = gets.chomp
-    break if valid_loan_duration?(loan_duration_years)
-    puts "Invalid duration."
-    puts error
-  end
+  loan_amount = get_loan_amount
+  apr = get_apr
+  loan_duration_years = get_loan_duration
 
   # prepare input variables for calculation
-  loan_amount = loan_amount.to_f
-  apr = apr.to_f / 100 # this decimalises the APR
-  loan_duration_months = loan_duration_years.to_f * 12
-  monthly_interest_rate = apr / 12
+  loan_duration_months = loan_duration_years.to_f * MONTHS_IN_YEAR
+  monthly_interest_rate = apr / MONTHS_IN_YEAR
 
   # main calculation of monthly payment
   if apr == 0
     monthly_payment = loan_amount / loan_duration_months
   else
-    monthly_payment = loan_amount *
-                      (monthly_interest_rate / (1 -
-                      (1 + monthly_interest_rate)**(-loan_duration_months)))
+    monthly_payment = calculate_monthly_payment(loan_amount, loan_duration_months, monthly_interest_rate)
   end
 
   # output results, rounded up to 0 or 2 d.p.
   result = <<~RESULT
-
+  
   The amount to borrow is $#{format('%.2f', loan_amount)}
   The APR is #{(apr * 100).round(2)}%
   There will be #{loan_duration_months.round(0)} monthly repayments
