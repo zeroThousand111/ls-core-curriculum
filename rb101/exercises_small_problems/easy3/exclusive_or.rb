8f838f3043d5da0c3c13ae7c59334fe7178a878f
Graham Jarvis <zeroThousand111@proton.me>
2023-05-02 10:27:19 +0000

    add falsy and truthy tests to exclusive_or.rb

--- rb101/exercises_small_problems/easy3/exclusive_or.rb
+++ rb101/exercises_small_problems/easy3/exclusive_or.rb
@@ -1,12 +1,7 @@
-# # my method
+# my method
 # def xor?(arg1, arg2)
 #   arg1 != arg2
 # end
-
-def xor?(value1, value2)
-  !value1 != !value2
-end
-
 
 # official solution
 def xor?(value1, value2)
@@ -15,7 +10,26 @@ def xor?(value1, value2)
   false
 end
 
-p xor?(5.even?, 4.even?) == true
-p xor?(5.odd?, 4.odd?) == true
-p xor?(5.odd?, 4.even?) == false
-p xor?(5.even?, 4.odd?) == false
+# Chad Gratt's Solution
+# def xor?(value1, value2)
+#   !value1 != !value2
+# end
+
+# my tests
+# boolean tests
+p xor?(true, true) == false
+p xor?(false, false) == false
+p xor?(true, false) == true
+p xor?(false, true) == true
+
+# truthy and falsy tests
+p xor?(nil, true) == true
+p xor?(nil, false) == false
+p xor?(3, false) == true
+p xor?(3, true) == false
+
+# problem tests
+# p xor?(5.even?, 4.even?) == true
+# p xor?(5.odd?, 4.odd?) == true
+# p xor?(5.odd?, 4.even?) == false
+# p xor?(5.even?, 4.odd?) == false
