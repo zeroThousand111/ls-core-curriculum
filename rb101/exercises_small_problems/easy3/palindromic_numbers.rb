5e795f5ad7ba186e45466c35c13fd341b9b05a75
Graham Jarvis <zeroThousand111@proton.me>
2023-05-03 11:24:28 +0000

    extended palindromic_numbers.rb with more tests for further exploration

--- rb101/exercises_small_problems/easy3/palindromic_numbers.rb
+++ rb101/exercises_small_problems/easy3/palindromic_numbers.rb
@@ -1,10 +1,22 @@
 # definition
+# def palindromic_number?(n)
+#   n.to_s.reverse == n.to_s
+# end
+
+# tests
+# p palindromic_number?(34543) == true
+# p palindromic_number?(123210) == false
+# p palindromic_number?(22) == true
+# p palindromic_number?(5) == true
+
+# further exploration method
 def palindromic_number?(n)
   n.to_s.reverse == n.to_s
 end
 
-# tests
-p palindromic_number?(34543) == true
-p palindromic_number?(123210) == false
-p palindromic_number?(22) == true
-p palindromic_number?(5) == true
+
+# further exploration tests
+p palindromic_number?(012321) == false
+p palindromic_number?(0123210) == true
+p palindromic_number?(010) == true
+p palindromic_number?(00100) == true
