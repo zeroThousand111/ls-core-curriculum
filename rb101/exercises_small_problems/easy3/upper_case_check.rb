d8ce5a2aa731da7a33e0ad9aed3508e386a9d044
Graham Jarvis <zeroThousand111@proton.me>
2023-05-03 12:43:34 +0000

    create upper_case_check.rb to answer exercise in the easy3 set

---
+++ rb101/exercises_small_problems/easy3/upper_case_check.rb
@@ -0,0 +1,12 @@
+# method
+def uppercase?(string)
+  string.upcase == string
+end
+
+# tests
+p uppercase?('t') == false
+p uppercase?('T') == true
+p uppercase?('Four Score') == false
+p uppercase?('FOUR SCORE') == true
+p uppercase?('4SCORE!') == true
+p uppercase?('') == true
