00c28dc34fa2e64160c46d1d0aae6fb15ceb5076
Graham Jarvis <zeroThousand111@proton.me>
2023-05-05 18:28:28 +0000

    create rps_bonus_features.rb and implement first bonus feature

---
+++ rb101/lesson2/rps_bonus_features.rb
@@ -0,0 +1,51 @@
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def win?(first, second)
+  winning_conditions = {
+  'rock'=>['scissors', 'lizard'],
+  'paper'=>['rock', 'spock'],
+  'scissors'=>['paper', 'lizard'],
+  'lizard'=>['spock', 'paper'],
+  'spock'=>['rock', 'scissors']
+  }
+  winning_conditions[first].include?(second)
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  puts "You chose #{choice}; Computer chose #{computer_choice}"
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for playing. Good bye!")
