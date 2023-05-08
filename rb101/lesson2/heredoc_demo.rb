f4b8f478b331c5fee5e7556e91b032c69a20c98a
Graham Jarvis <zeroThousand111@proton.me>
2023-05-08 08:19:51 +0000

    add different heredoc types to heredoc_demo.rb

--- rb101/lesson2/heredoc_demo.rb
+++ rb101/lesson2/heredoc_demo.rb
@@ -1,26 +1,63 @@
+string = "string"
+
 # basic heredoc
 basic = <<BASIC
+
 This is a basic heredoc.
-I can write.
-Over multiple lines.
+  I can write.
+    Over multiple lines.
+(But if I indent the terminator,
+it doesn't work!)
+
 BASIC
 
-# indented heredoc
+# indented terminator heredoc
 indented = <<-INDENT
 This is an indented heredoc.
-To be honest I'm not sure.
-What the difference is.
-INDENT
+  And it is OK for me
+    to indent the terminator.
+I can also use #{string} interpolation.
+  INDENT
 
 # squiggly heredoc
 squiggly = <<~SQUIGGLY
-This is a squiggly heredoc.
-I can see why they call it that.
-But I don't know what it does.
+
+    This is a squiggly heredoc.
+     It strips any leading white space
+      on the first line from all lines.
+      #{string} test
 SQUIGGLY
 
+# percent string literal
+percent_string_literal = %Q(
+
+  This is a heredoc
+    that has been created
+      from a percent string literal
+
+)
+
+# double quotes heredoc allows string interpolation
+double_quotes = <<"DOUBLE"
+
+  A double quoted heredoc can
+    also use #{string} interpolation.
+      One plus one is #{1 + 1}
+    
+DOUBLE
+
+# single quotes is the same as a regular heredoc
+single_quotes =<<'SINGLE'
+  This is a single quote heredoc
+    I can use special characters without them
+meaning anything special #{no string interpolation here}
+  one plus one is #{1 + 1}
+  
+SINGLE
+
 puts basic
-puts ""
 puts indented
-puts ""
 puts squiggly
+puts percent_string_literal
+puts double_quotes
+puts single_quotes
