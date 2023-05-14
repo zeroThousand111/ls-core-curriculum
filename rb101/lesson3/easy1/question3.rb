2c3db547506f49c99a4e6b0648641c47f1a8cafc
Graham Jarvis <zeroThousand111@proton.me>
2023-05-14 17:27:54 +0000

    create question3.rb to answer easy1 in lesson 3 of RB101

---
+++ rb101/lesson3/easy1/question3.rb
@@ -0,0 +1,6 @@
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+split_string = advice.split(' ')
+split_string[6] = 'urgent'
+restored_string = split_string.join(' ')
+puts restored_string
