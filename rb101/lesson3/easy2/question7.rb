dac71c12e4e5b1cd759c5386d0a4615ab5dedca5
Graham Jarvis <zeroThousand111@proton.me>
2023-05-15 13:21:06 +0000

    create question7.rb for easy2 lesson 3 RB101

---
+++ rb101/lesson3/easy2/question7.rb
@@ -0,0 +1,11 @@
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+# push method with multiple arguments
+p flintstones.push("Dino", "Hoppy")
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+p flintstones
+
+# concat method
+p flintstones.concat(["Dino", "Hoppy"])
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+p flintstones
