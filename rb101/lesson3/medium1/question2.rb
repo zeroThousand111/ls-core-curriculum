cd1515f9327e9631d6e3af1ea2935b7cf294a7a2
Graham Jarvis <zeroThousand111@proton.me>
2023-05-16 08:07:10 +0000

    create question2.rb for medium1 in lesson 3 RB101

---
+++ rb101/lesson3/medium1/question2.rb
@@ -0,0 +1,7 @@
+# puts "the value of 40 + 2 is " + (40 + 2)
+
+puts "the value of 40 + 2 is #{40 + 2}"
+
+puts "the value of 40 + 2 is " + "#{40 + 2}"
+
+puts "the value of 40 + 2 is " + (40 + 2).to_s
