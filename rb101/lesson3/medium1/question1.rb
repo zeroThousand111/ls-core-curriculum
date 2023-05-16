be4782a8eff9e93390f860e68ea2c5d61d300ec0
Graham Jarvis <zeroThousand111@proton.me>
2023-05-16 07:54:46 +0000

    add second solution to question 1 medium1 lesson3 RB101

--- rb101/lesson3/medium1/question1.rb
+++ rb101/lesson3/medium1/question1.rb
@@ -1,2 +1,5 @@
 10.times { |i| puts "The Flintstones Rock!".rjust(21 + i)}
 
+10.times { |number| puts (" " * number) + "The Flintstones Rock!" }
+
+10.times { |i| puts (" " * i) + "The Flintstones Rock!" }
