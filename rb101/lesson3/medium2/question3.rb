e744b52dfa3e38291393684660fb7b5d0ccf3b6c
Graham Jarvis <zeroThousand111@proton.me>
2023-05-17 14:38:28 +0000

    create question3.rb for medium2 lesson 3 RB101

---
+++ rb101/lesson3/medium2/question3.rb
@@ -0,0 +1,11 @@
+def tricky_method(string_param_one, string_param_two)
+  string_param_one += "rutabaga"
+  string_param_two << "rutabaga"
+end
+
+string_arg_one = "pumpkins"
+string_arg_two = "pumpkins"
+tricky_method(string_arg_one, string_arg_two)
+
+puts "String_arg_one looks like this now: #{string_arg_one}"
+puts "String_arg_two looks like this now: #{string_arg_two}"
