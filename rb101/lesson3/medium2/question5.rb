7738c8089b7e24702b4765b02d8ee04bb472156e
Graham Jarvis <zeroThousand111@proton.me>
2023-05-17 17:31:17 +0000

    refactor question5.rb to reassign both variables in separate methods

--- rb101/lesson3/medium2/question5.rb
+++ rb101/lesson3/medium2/question5.rb
@@ -1,11 +1,31 @@
-def tricky_method(a_string_param, an_array_param)
-  a_string_param += "rutabaga"
-  an_array_param << "rutabaga"
+# original code
+
+# def tricky_method(a_string_param, an_array_param)
+#   a_string_param += "rutabaga"
+#   an_array_param << "rutabaga"
+# end
+
+# my_string = "pumpkins"
+# my_array = ["pumpkins"]
+# tricky_method(my_string, my_array)
+
+# puts "My string looks like this now: #{my_string}"
+# puts "My array looks like this now: #{my_array}"
+
+# new code that doesn't mutate arguments
+
+def tricky_method_string(a_string_param)
+  a_string_param = "pumpkinsrutabaga"
+end
+
+def tricky_method_array(an_array_param)
+  an_array_param = ["pumpkins", "rutabaga"]
 end
 
 my_string = "pumpkins"
 my_array = ["pumpkins"]
-tricky_method(my_string, my_array)
+my_string = tricky_method_string(my_string)
+my_array = tricky_method_array(my_array)
 
 puts "My string looks like this now: #{my_string}"
 puts "My array looks like this now: #{my_array}"
