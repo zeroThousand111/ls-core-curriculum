b887774d0f41a4511d04a65707ce8c020bbb4eab
Graham Jarvis <zeroThousand111@proton.me>
2023-05-18 17:14:01 +0000

    create question3b.rb for hard1 lesson3 RB101

---
+++ rb101/lesson3/hard1/question3b.rb
@@ -0,0 +1,15 @@
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
