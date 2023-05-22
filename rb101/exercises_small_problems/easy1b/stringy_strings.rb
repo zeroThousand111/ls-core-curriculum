62b426ad6e5e63f4e41bdd22408d9c2490067e5d
Graham Jarvis <zeroThousand111@proton.me>
2023-05-22 13:22:57 +0000

    update stringy_strings.rb to solve further exploration

--- rb101/exercises_small_problems/easy1b/stringy_strings.rb
+++ rb101/exercises_small_problems/easy1b/stringy_strings.rb
@@ -1,14 +1,26 @@
 # method
-def stringy(n)
+def stringy(n, rev = 1)
   array = []
-  n.times do | i |
-    array << (i.even? ? 1 : 0)
+  if rev == 1
+    n.times do | i |
+      array << (i.even? ? 1 : 0)
+    end
+  else
+    n.times do | i |
+      array << (i.odd? ? 1 : 0)
+    end
   end
   array.join
 end
 
-# tests
-puts stringy(6) == '101010'
-puts stringy(9) == '101010101'
-puts stringy(4) == '1010'
-puts stringy(7) == '1010101'
+# tests for main problem
+# puts stringy(6) == '101010'
+# puts stringy(9) == '101010101'
+# puts stringy(4) == '1010'
+# puts stringy(7) == '1010101'
+
+# tests for further exploration
+puts stringy(6, 0) == '010101'
+puts stringy(9, 1) == '101010101'
+puts stringy(4, 0) == '0101'
+puts stringy(7, 1) == '1010101'
