f091c3819078ef905daa9e60a1464807e19717bd
Graham Jarvis <zeroThousand111@proton.me>
2023-05-23 13:50:48 +0000

    update right_triangles for further exploration

--- rb101/exercises_small_problems/easy1b/right_triangles.rb
+++ rb101/exercises_small_problems/easy1b/right_triangles.rb
@@ -1,6 +1,40 @@
+# first solution
 def triangle(n)
   n.times do |i|
     puts (" " * (n - (i + 1))) + ("*" * (i + 1))
+  end
+end
+
+# official solution
+# def triangle(num)
+#   spaces = num - 1
+#   stars = 1
+
+#   num.times do |n|
+#     puts (' ' * spaces) + ('*' * stars)
+#     spaces -= 1
+#     stars += 1
+#   end
+# end
+
+# inverted right-left triangle
+def inv_triangle(n)
+  n.times do |i|
+    puts (" " * i) + ("*" * (n - i))
+  end
+end
+
+# left-right triangle
+def left_right_triangle(n)
+  n.times do |i|
+    puts ("*" * (i + 1))
+  end
+end
+
+# inverted left-right triangle
+def inv_left_right_triangle(n)
+  n.times do |i|
+    puts ("*" * (n - i))
   end
 end
 
@@ -8,3 +42,15 @@ end
 triangle(3)
 triangle(5)
 triangle(9)
+
+inv_triangle(3)
+inv_triangle(5)
+inv_triangle(9)
+
+left_right_triangle(3)
+left_right_triangle(5)
+left_right_triangle(9)
+
+inv_left_right_triangle(3)
+inv_left_right_triangle(5)
+inv_left_right_triangle(9)
