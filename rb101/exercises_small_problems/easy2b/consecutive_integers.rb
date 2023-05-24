5593a418749dd65f30b9e7e037cfaf783a221dea
Graham Jarvis <zeroThousand111@proton.me>
2023-05-24 11:27:48 +0000

    refactor consecutive_integers.rb to include validation methods and inject methods

--- rb101/exercises_small_problems/easy2b/consecutive_integers.rb
+++ rb101/exercises_small_problems/easy2b/consecutive_integers.rb
@@ -5,19 +5,47 @@ def calculation(n, operation)
     array << i + 1
   end
   if operation == 's'
-    return array.sum
+    return array.inject(:+)
   elsif operation == 'p'
     return array.inject(:*)
   end
 end
 
+def valid_integer?(n)
+  n.to_s.to_i == n && n > 0
+end
+
+def valid_operator?(string)
+  string.include?('s') || string.include?('p')
+end
+
+def get_integer
+  int = nil
+  loop do
+    puts ">> Please enter an integer greater than 0:"
+    int = gets.chomp.to_i
+    break if valid_integer?(int)
+    puts ">> Sorry that's not a valid integer above zero.  Please try again."
+  end
+  int
+end
+
+def get_operator
+  calc = nil
+  loop do 
+    puts "> >> Enter 's' to compute the sum, 'p' to compute the product."
+    calc = gets.chomp.downcase!
+    break if valid_operator?(calc)
+    puts ">> Sorry that's not a valid input.  Please type 's' or 'p'"
+  end
+  calc
+end
+
 total = 0
 
 # get user input
-puts ">> Please enter an integer greater than 0:"
-integer = gets.chomp.to_i
-puts "> >> Enter 's' to compute the sum, 'p' to compute the product."
-calc = gets.chomp
+integer = get_integer
+calc = get_operator
 
 # invoke method on the user input
 total = calculation(integer, calc)
