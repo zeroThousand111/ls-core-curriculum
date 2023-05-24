41a59b34347f152a12d974717e30a2e7fa18700d
Graham Jarvis <zeroThousand111@proton.me>
2023-05-24 15:32:48 +0000

    create palindromic_strings_part_1.rb

---
+++ rb101/exercises_small_problems/easy3b/palindromic_strings_part_1.rb
@@ -0,0 +1,9 @@
+def palindrome?(string)
+  string == string.reverse
+end
+
+# tests
+p palindrome?('madam') == true
+p palindrome?('Madam') == false          # (case matters)
+p palindrome?("madam i'm adam") == false # (all characters matter)
+p palindrome?('356653') == true
