7365ab5ce7ada346383575e358c2e2edc9fa64be
Graham Jarvis <zeroThousand111@proton.me>
2023-05-25 15:46:22 +0000

    update palindromic_numbers.rb to account for further exploration

--- rb101/exercises_small_problems/easy3b/palindromic_numbers.rb
+++ rb101/exercises_small_problems/easy3b/palindromic_numbers.rb
@@ -1,7 +1,7 @@
 # string method
-def palindromic_number?(n)
-  n.to_s == n.to_s.reverse
-end
+# def palindromic_number?(n)
+#   n.to_s == n.to_s.reverse
+# end
 
 # array method
 # def palindromic_number?(n)
@@ -10,8 +10,22 @@ end
 #   array == reverse_array
 # end
 
+# further exploration
+def palindromic_number?(n)
+  
+end
+
 # tests
-p palindromic_number?(34543) == true
-p palindromic_number?(123210) == false
-p palindromic_number?(22) == true
-p palindromic_number?(5) == true
+# p palindromic_number?(34543) == true
+# p palindromic_number?(123210) == false
+# p palindromic_number?(22) == true
+# p palindromic_number?(5) == true
+
+# further exploration tests with leading zeros
+# p palindromic_number?(0123210) == true
+# p palindromic_number?(022) == false
+# p palindromic_number?(050) == true
+
+p palindromic_number?(0123210)
+p palindromic_number?(022)
+p palindromic_number?(050)
