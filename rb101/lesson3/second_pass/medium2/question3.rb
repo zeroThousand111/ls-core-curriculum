55cc49f3eb0f7cceb2eb29fbe6692f9de460aaba
Graham Jarvis <zeroThousand111@proton.me>
2023-06-27 15:04:01 +0000

    create question3.rb for medium2 problems in lesson 3 RB101

---
+++ rb101/lesson3/second_pass/medium2/question3.rb
@@ -0,0 +1,15 @@
+def tricky_method(string_param_one, string_param_two)
+  string_param_one += "rutabaga"
+  string_param_two << "rutabaga"
+end
+
+string_arg_one = "pumpkins"
+string_arg_two = "pumpkins"
+tricky_method(string_arg_one, string_arg_two)
+
+puts "String_arg_one looks like this now: #{string_arg_one}"
+puts "String_arg_two looks like this now: #{string_arg_two}"
+
+# what will be displayed and why?
+# String_arg_one looks like this now: pumpkins
+# String_arg_two looks like this now: pumpkinsrutabaga
