b8ed44a7dc858f2aa959e923183020c9cb00772e
Graham Jarvis <zeroThousand111@proton.me>
2023-06-28 10:20:26 +0000

    create question6.rb for easy1 problems in lesson 3 RB101

---
+++ rb101/lesson3/second_pass/easy1/question6.rb
@@ -0,0 +1,9 @@
+# show two different ways to put the expected "Four score and " in front of it.
+
+famous_words = "seven years ago..."
+
+puts "Four score and #{famous_words}"
+
+puts "Four score and " + famous_words
+
+puts famous_words.prepend("Four score and ")
