d9013dd7eb74ca13738b6186b6c4c541d9ed908b
Graham Jarvis <zeroThousand111@proton.me>
2023-06-28 10:32:12 +0000

    create question8.rb for easy1 problems in lesson 3 RB101

---
+++ rb101/lesson3/second_pass/easy1/question8.rb
@@ -0,0 +1,9 @@
+#  Create an array containing only two elements: Barney's name and Barney's number.
+
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+
+p array = (flintstones.select { |key, value| key == "Barney" }).to_a
+
+# official solution
+
+p flintstones.assoc("Barney")
