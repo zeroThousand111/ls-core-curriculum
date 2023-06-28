65fb0409080e78683bd8e88ccc9e36c7a772e6b7
Graham Jarvis <zeroThousand111@proton.me>
2023-06-28 10:39:19 +0000

    update question 8.rb in lesson 3 RB101 to explore my previous first pass solution

--- rb101/lesson3/second_pass/easy1/question8.rb
+++ rb101/lesson3/second_pass/easy1/question8.rb
@@ -4,6 +4,10 @@ flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam"
 
 p array = (flintstones.select { |key, value| key == "Barney" }).to_a
 
+# my previous solution
+
+p array = [flintstones.key(2), flintstones["Barney"]]
+
 # official solution
 
 p flintstones.assoc("Barney")
