ce17860ad5593db02be2015d8bd58375aa694219
Graham Jarvis <zeroThousand111@proton.me>
2023-06-28 10:14:54 +0000

    create question3.rb for easy1 problems in lesson 3 RB101

---
+++ rb101/lesson3/second_pass/easy1/question3.rb
@@ -0,0 +1,6 @@
+# Replace the word "important" with "urgent" in this string:
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+advice.gsub!("important", "urgent")
+puts advice
