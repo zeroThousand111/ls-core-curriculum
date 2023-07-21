3de0e2da731dcbe15b622ec71009b29db9403f96
Graham Jarvis <zeroThousand111@proton.me>
2023-07-21 15:17:12 +0000

    create question5.rb in Medium1 set for third pass of Lesson 3 in RB101

---
+++ rb101/lesson3/third_pass/medium1/question5.rb
@@ -0,0 +1,19 @@
+=begin
+Alyssa asked Ben to write up a basic implementation of a Fibonacci calculator. A user passes in two numbers, and the calculator will keep computing the sequence until some limit is reached.
+
+Ben coded up this implementation but complained that as soon as he ran it, he got an error. Something about the limit variable. What's wrong with the code?
+=end
+
+limit = 15
+
+def fib(first_num, second_num, limit)
+  while first_num + second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(0, 1, limit)
+puts "result is #{result}"
