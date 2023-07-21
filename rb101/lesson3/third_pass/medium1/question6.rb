52fe8f36cca427ca5823c3d5dacda5000f60d2f9
Graham Jarvis <zeroThousand111@proton.me>
2023-07-21 15:19:26 +0000

    create question6.rb in Medium1 set for third pass of Lesson 3 in RB101

---
+++ rb101/lesson3/third_pass/medium1/question6.rb
@@ -0,0 +1,15 @@
+answer = 42
+
+def mess_with_it(some_number)
+  some_number += 8
+end
+
+new_answer = mess_with_it(answer)
+
+p answer - 8
+
+# what is the output of the code?
+# the output will be:
+# 34
+
+p new_answer
