84d3ce958478dfcfdf4ec9c4f8ef2643b2ee4802
Graham Jarvis <zeroThousand111@proton.me>
2023-07-21 11:03:54 +0000

    create question2.rb in Medium1 set for third pass of Lesson 3 in RB101

---
+++ rb101/lesson3/third_pass/medium1/question2.rb
@@ -0,0 +1,7 @@
+# puts "the value of 40 + 2 is " + (40 + 2)
+
+# fix 1 - string interpolation
+puts "the value of 40 + 2 is #{40 + 2}"
+
+# fix 2 - type conversion
+puts "the value of 40 + 2 is " + (40 + 2).to_s
