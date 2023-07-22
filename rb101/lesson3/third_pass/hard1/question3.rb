a2b5eace1dcb05ee0ca81803f8a0fb4851f8dd32
Graham Jarvis <zeroThousand111@proton.me>
2023-07-22 08:40:19 +0000

    create question3.rb in Hard1 set for third pass of Lesson 3 in RB101

---
+++ rb101/lesson3/third_pass/hard1/question3.rb
@@ -0,0 +1,50 @@
+# A
+def mess_with_vars(one, two, three)
+  one = two
+  two = three
+  three = one
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}" # one
+puts "two is: #{two}" # two
+puts "three is: #{three}" # three
+
+# B
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}" # one
+puts "two is: #{two}" # two
+puts "three is: #{three}" # three
+
+# C
+def mess_with_vars(one, two, three)
+  one.gsub!("one","two")
+  two.gsub!("two","three")
+  three.gsub!("three","one")
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}" # two
+puts "two is: #{two}" # three
+puts "three is: #{three}" # one
