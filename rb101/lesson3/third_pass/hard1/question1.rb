8dd846d7e9f7ffb5cfbe853964584db1681caf33
Graham Jarvis <zeroThousand111@proton.me>
2023-07-22 08:21:57 +0000

    create question1.rb in Hard1 set for third pass of Lesson 3 in RB101

---
+++ rb101/lesson3/third_pass/hard1/question1.rb
@@ -0,0 +1,43 @@
+# if false
+#   greeting = "hello world"
+# end
+
+# p greeting
+
+# if true
+#   greeting = "hello world"
+# else
+#   something_other = "hello dolly"
+# end
+
+# p greeting
+# p something_other
+
+
+
+# if true
+#   greeting = "hello world"
+# else
+#   something_other = "hello dolly"
+# end
+
+# p greeting
+# p something_other
+
+
+
+thing = 5
+
+case thing
+  when 6
+    greeting = "hello world"
+  when 5
+    something_other = "hello dolly"
+  when 7
+    something_else = "goodbye cruel world"
+end
+
+p greeting
+p something_other
+p something_else
+  
