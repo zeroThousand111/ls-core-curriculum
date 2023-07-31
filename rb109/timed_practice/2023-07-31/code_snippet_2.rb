71fa660e94dda6d5bf47f40292245854ab2d1764
Graham Jarvis <zeroThousand111@proton.me>
2023-07-31 13:12:06 +0000

    create code_snippet_2.rb for timed exercise practice on 31st July 2023

---
+++ rb109/timed_practice/2023-07-31/code_snippet_2.rb
@@ -0,0 +1,10 @@
+def short_long_short(string1, string2)
+  if string1.length > string2.length
+    string2 + string1 + string2
+  else
+    string1 + string2 + string1
+  end
+end
+
+longer_string = short_long_short('abc', 'defgh')
+p longer_string
