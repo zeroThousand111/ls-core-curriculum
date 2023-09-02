b15f451ec3af0893bdeffe2a12726a6eddd3526b
Graham Jarvis <zeroThousand111@proton.me>
2023-09-02 11:49:53 +0000

    create odd.rb for easy1 set of small problems

---
+++ rb101/exercises_small_problems/easy1b/odd.rb
@@ -0,0 +1,11 @@
+def is_odd?(number)
+  number % 2 == 1
+end
+
+# truth tests
+puts is_odd?(2) == false
+puts is_odd?(5) == true
+puts is_odd?(-17) == true
+puts is_odd?(-8) == false
+puts is_odd?(0) == false
+puts is_odd?(7) == true
