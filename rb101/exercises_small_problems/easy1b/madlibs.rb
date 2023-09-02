b94aec358ea0dd5f5a14232d22e1add55644b444
Graham Jarvis <zeroThousand111@proton.me>
2023-09-02 11:56:14 +0000

    create madlibs.rb for easy1 set of small problems

---
+++ rb101/exercises_small_problems/easy1b/madlibs.rb
@@ -0,0 +1,11 @@
+
+puts "Enter a noun:"
+noun = gets.chomp
+puts "Enter a verb:"
+verb = gets.chomp
+puts "Enter an adjective:"
+adjective = gets.chomp
+puts "Enter an adverb:"
+adverb = gets.chomp
+
+puts "Do you #{verb} your #{adjective} #{noun} #{adverb}?  Really?"
