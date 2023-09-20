06877b251684ebedc26d4441e34ec7d83daaa9b8
Graham Jarvis <zeroThousand111@proton.me>
2023-09-20 16:18:09 +0000

    update triangle.rb to answer bonus challenge

--- rb101/exercises_small_problems/easy1c/triangle.rb
+++ rb101/exercises_small_problems/easy1c/triangle.rb
@@ -1,4 +1,4 @@
-def triangle(n)
+def triangle_south_east(n)
   spaces = n - 1
   stars = 1
   n.times do
@@ -8,5 +8,50 @@ def triangle(n)
   end
 end
 
-triangle(3)
-triangle(9)
+def triangle_south_west(n)
+  spaces = n - 1
+  stars = 1
+  n.times do
+    puts ("*" * stars) + (" " * spaces)
+    spaces -= 1
+    stars += 1
+  end
+end
+
+def triangle_north_west(n)
+  spaces = 1
+  stars = n - 1
+  n.times do
+    puts ("*" * stars) + (" " * spaces)
+    spaces += 1
+    stars -= 1
+  end
+end
+
+def triangle_north_west(n)
+  spaces = 0
+  stars = n
+  n.times do
+    puts ("*" * stars) + (" " * spaces)
+    spaces += 1
+    stars -= 1
+  end
+end
+
+def triangle_north_east(n)
+  spaces = 0
+  stars = n
+  n.times do
+    puts  (" " * spaces) + ("*" * stars)
+    spaces += 1
+    stars -= 1
+  end
+end
+
+triangle_south_east(5)
+puts ""
+triangle_south_west(5)
+puts ""
+triangle_north_west(5)
+puts ""
+triangle_north_east(5)
