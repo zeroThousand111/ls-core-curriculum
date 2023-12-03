54b0204e4defe886469cb178771a341a035fb970
Graham Jarvis <zeroThousand111@proton.me>
2023-12-03 17:22:01 +0000

    finish alphabetical_numbers.rb in small problems exercises easy5 set

--- rb110/exercises_small_problems/easy5/alphabetical_numbers.rb
+++ rb110/exercises_small_problems/easy5/alphabetical_numbers.rb
@@ -21,7 +21,7 @@ Output:
 Explicit reqs:
   # What does the problem say about what you should do?
   - Write a method;
-  - The input is always an array of consecutive Integers from zero to 19;
+  - The input is always an array of Integers from zero to 19;
   - Sort according to the English spelling of each number;
   
 Implicit reqs:
@@ -56,7 +56,12 @@ Step-by-step process (plaintext or pseudocode):
   # The logical process by which you intend to tackle the problem at hand
   
   High Level
-  1. Create a translation dictionary between Integers and their corresponding English names;
+  1. Create a translation dictionary hash between Integers and their corresponding English names;
+  2. Create a new list of alphabetic numbers that matches the input list of Integers;
+  3. Sort this alpabetic list alphabetically
+  4. Create a new list of numbers in the same order as the alphabetic list and return it.
+  
+  
   2. Make a list of names in alpabetic order
     a. Make a list of the Integer names;
     b. Sort the list of names into alphabetical order;
@@ -76,6 +81,8 @@ ________________________________________________________________________________
 Time to code!
 =end
 
+# solution 1: Long version
+
 # 1. Create a DICTIONARY hash of key-value pairs
 
 DICTIONARY = {
@@ -87,35 +94,63 @@ DICTIONARY = {
   5 => "five",
   6 => "six",
   7 => "seven",
+  8 => "eight",
+  9 => "nine",
+  10 => "ten",
+  11 => "eleven",
+  12 => "twelve",
+  13 => "thirteen",
+  14 => "fourteen",
+  15 => "fifteen",
+  16 => "sixteen",
+  17 => "seventeen",
+  18 => "eighteen",
+  19 => "nineteen",
 }
 
+# def alphabetic_number_sort(input_array)
+#   # 2. Create a new list of alphabetic numbers that matches the input list of Integers;
+#   alphabetic_array = input_array.map do |num|
+#     DICTIONARY[num]
+#   end
+  
+#   # 3. Sort this alpabetic list alphabetically
+  
+#   sorted_array = alphabetic_array.sort
+  
+#   # 4. Create a new list of numbers in the same order as the alphabetic list and return it.
+  
+#   sorted_numbers_array = sorted_array.map do |word|
+#     DICTIONARY.key(word)
+#   end
+# end
+
+# solution 2: refactored to be shorter
 def alphabetic_number_sort(input_array)
-  # 2. create a list of Integer names and sort them alphabetically
-  sorted_names_array = DICTIONARY.values.sort
+  alphabetic_array_sorted = input_array.map { |num| DICTIONARY[num] }.sort
   
-  # 3. create a corresponding list of numbers in that same order
-  sorted_integers_array = sorted_names_array.map do |name|
-                              DICTIONARY.key(name)
-                            end
-                            
-  # 4. create a blank list as long as the input_array
-  p blank_list = Array.new(input_array.length)
-  
-  # 5. For EACH item of the input_array, overwrite the item on the blank list at the index order of the Integer in the sorted list of names
-  
-  input_array.each do |i|
-    blank_list[sorted_integers_array.index(i)] = i
-  end
-  # 6. Return the completed list
-  blank_list
+  alphabetic_array_sorted.map { |word|DICTIONARY.key(word) }
 end
 
-# print test
-p alphabetic_number_sort((0..2).to_a)
-# p alphabetic_number_sort((0..4).to_a)
+
+# solution 3: Official LS solution using #sort_by
+
+# NUMBER_WORDS = %w(zero one two three four
+#                   five six seven eight nine
+#                   ten eleven twelve thirteen fourteen
+#                   fifteen sixteen seventeen eighteen nineteen)
+
+# def alphabetic_number_sort(numbers)
+#   numbers.sort_by { |number| NUMBER_WORDS[number] }
+# end
+
+# print tests
+p alphabetic_number_sort((0..2).to_a) 
+# [1, 2, 0]
+p alphabetic_number_sort((0..4).to_a)
 # [5, 4, 1, 3, 2, 0]
-# p alphabetic_number_sort((0..19).to_a)
+p alphabetic_number_sort((0..19).to_a)
 # [8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,6, 16, 10, 13, 3, 12, 2, 0]
   
 # truth test
-# p alphabetic_number_sort((0..19).to_a) == [8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17, 6, 16, 10, 13, 3, 12, 2, 0]
+p alphabetic_number_sort((0..19).to_a) == [8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17, 6, 16, 10, 13, 3, 12, 2, 0]
