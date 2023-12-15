171bbe3e2adcd5670b40ae5a0e2c2e21e4541a6e
Graham Jarvis <zeroThousand111@proton.me>
2023-12-15 12:15:45 +0000

    create practice_problem_5.rb for RB110 Lesson 2 Practice Problems

---
+++ rb110/lesson2/practice_problems/practice_problem_5.rb
@@ -0,0 +1,17 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+age = 0
+
+munsters.each do |key, value|
+  if value["gender"] == "male"
+    age += value["age"]
+  end
+end
+
+p age # => 444
