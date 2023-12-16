6a78f3e97bd094a974e7ba7ee0c6ecfa65645542
Graham Jarvis <zeroThousand111@proton.me>
2023-12-16 08:13:33 +0000

    update practice_problem_9.rb in RB110 Lesson 2 Practice Problems to create descending order of sub-arrays

--- rb110/lesson2/practice_problems/practice_problem_9.rb
+++ rb110/lesson2/practice_problems/practice_problem_9.rb
@@ -1,5 +1,9 @@
 arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']]
 
-p arr.map { |sub_array| sub_array.sort }
+p (arr.map do |sub_array| 
+  sub_array.sort do | a, b |
+    b <=> a
+  end
+end)
 
 # => ["a", "b", "c"], [1, 2, 3], ["black", "blue", "green"]]
