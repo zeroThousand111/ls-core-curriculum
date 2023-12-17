8895d1c2697321e144f7a87f0dcc87e14ee889cb
Graham Jarvis <zeroThousand111@proton.me>
2023-12-17 18:59:32 +0000

    update capitalize_words.rb for further exploration

--- rb110/exercises_small_problems/easy7/capitalize_words.rb
+++ rb110/exercises_small_problems/easy7/capitalize_words.rb
@@ -65,20 +65,84 @@ Now CODE with intent!
 
 # solution 2 - using #map
 
-def word_cap(input_string)
-  input_string.split.map { |word| word.capitalize }.join(' ')
-end
+# def word_cap(input_string)
+#   input_string.split.map { |word| word.capitalize }.join(' ')
+# end
 
 # solution 3 - official LS solution using abbreviation
 
-def word_cap(words)
-  words.split.map(&:capitalize).join(' ')
-end
+# def word_cap(words)
+#   words.split.map(&:capitalize).join(' ')
+# end
 
 # FURTHER EXPLORATION
 
 # Ruby conveniently provides the String#capitalize method to capitalize strings. Without that method, how would you solve this problem? Try to come up with at least two solutions.
 
+# solution 4 - using a simple loop and string element reference conditional assignment
+
+# CAPS = ('A'..'Z').to_a
+# LOWS = ('a'..'z').to_a
+
+# def word_cap(input_string)
+#   array = input_string.split
+#   new_array = []
+#   word_counter = 0
+  
+#   loop do
+#     letter_counter = 1
+#     word = array[word_counter]
+#     new_word = ''
+#     new_word[0] = array[word_counter][0].upcase
+    
+#     loop do
+#       break if letter_counter == array[word_counter].size
+#       letter = word[letter_counter]
+#       if CAPS.include?(letter) || LOWS.include?(letter)
+#         new_word[letter_counter] = letter.downcase
+#       else
+#         new_word[letter_counter] = letter
+#       end
+#       letter_counter += 1
+#     end
+#     # p new_word
+#     new_array << new_word
+#     word_counter += 1
+#     break if word_counter == array.size
+#   end
+  
+#   new_array.join(' ')
+# end
+
+# solution 5 - Using #each to iterate through the same as solution 4 UNFINISHED
+
+CAPS = ('A'..'Z').to_a
+LOWS = ('a'..'z').to_a
+
+def word_cap(input_string)
+  array = input_string.split
+  new_array = []
+  
+  array.each do |word|
+    new_word_array = []
+    word.chars.each_with_index do |letter, index|
+      if CAPS.include?(letter) || LOWS.include?(letter)
+        if index == 0
+          new_word_array << letter.upcase
+        else
+          new_word_array << letter.downcase
+        end
+      else
+        new_word_array << letter
+      end
+      new_array << new_word_array.join
+    
+    end
+    
+  end
+  
+  new_array.join(' ')
+end
 
 # Print Tests
 p word_cap('the javaScript language') # 'The Javascript Language'
