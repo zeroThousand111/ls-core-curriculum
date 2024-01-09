7b3c64901d3336f7bf84bc3d8b57e017248dcd95
Graham Jarvis <zeroThousand111@proton.me>
2024-01-09 20:25:14 +0000

    update tic_tac_toe_walk_through.rb for video part 2

--- rb110/lesson3/tic_tac_toe_walk_through.rb
+++ rb110/lesson3/tic_tac_toe_walk_through.rb
@@ -1,20 +1,10 @@
-# tic tac toe
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
 
-=begin
-Higher-level pseudocode
-
-1. Display the initial empty 3x3 board.
-2. Ask the user to mark a square.
-3. Computer marks a square.
-4. Display the updated board state.
-5. If winner, display winner.
-6. If board is full, display tie.
-7. If neither winner nor board is full, go to #2
-8. Play again?
-9. If yes, go to #1
-10. Good bye!
-
-=end
+def prompt(msg)
+  puts "=> #{msg}"
+end
 
 def display_board(brd)
   puts ""
@@ -34,9 +24,29 @@ end
 
 def initialise_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = ' ' }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def player_places_piece!(brd)
+  square = ' '
+  loop do
+    prompt("choose a square (#{empty_squares(brd).join(', ')})")
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt("Sorry, that's not a valid choice")
+  end
+  
+  brd[square] = PLAYER_MARKER
+end
+
 board = initialise_board
+display_board(board)
+
+player_places_piece!(board)
+p board # checks the status of the hash `board`
 display_board(board)
