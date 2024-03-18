a391ce3ed57b3c305be7f4f2cad4d10d1edafd2d
Graham Jarvis <zeroThousand111@proton.me>
2024-03-18 14:00:50 +0000

    fibonacci_numbers_last_digit.rb update

--- rb119/exercises_small_problems/medium1/fibonacci_numbers_last_digit.rb
+++ rb119/exercises_small_problems/medium1/fibonacci_numbers_last_digit.rb
@@ -104,11 +104,11 @@ end
 # -------------------
 
 # Print Tests
-# p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
-# p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
-# p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
-# p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
-# p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
-p fibonacci_last(123456789) # -> 4
+p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+# p fibonacci_last(123456789) # -> 4
 
 # Test Cases
