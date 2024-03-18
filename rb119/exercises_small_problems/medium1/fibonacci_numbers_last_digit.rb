7b51b05671c84b041c5d763b41000cd7e5b1fe05
Graham Jarvis <zeroThousand111@proton.me>
2024-03-18 14:00:12 +0000

    create fibonacci_numbers_last_digit.rb for small problems exercises medium1 RB119

---
+++ rb119/exercises_small_problems/medium1/fibonacci_numbers_last_digit.rb
@@ -0,0 +1,114 @@
+=begin
+______________________________________________________________________________
+*****Understand the PROBLEM*****
+______________________________________________________________________________
+THE PROBLEM:
+In the previous exercise, we developed a procedural method for computing the value of the nth Fibonacci numbers. This method was really fast, computing the 20899 digit 100,001st Fibonacci sequence almost instantly.
+
+In this exercise, you are going to compute a method that returns the last digit of the nth Fibonacci number.
+
+INPUT:
+
+OUTPUT:
+
+RULES
+EXPLICIT REQUIREMENTS:
+  - 
+  -
+  -
+  
+IMPLICIT REQUIREMENTS:
+  -
+  -
+  -
+
+______________________________________________________________________________
+*****EXAMPLES/TEST CASES*****
+______________________________________________________________________________
+
+fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+fibonacci_last(123456789) # -> 4
+
+______________________________________________________________________________
+*****DATA STRUCTURES*****
+______________________________________________________________________________
+
+______________________________________________________________________________
+*****ALGORITHM*****
+______________________________________________________________________________
+1. Requirement — The important, generalized steps required to navigate from the input to the desired output.
+  a. Approach — The process that will be utilized to accomplish each Requirement.
+    i. Procedure — Specific details to implement the Approach.
+
+
+1.
+  a.
+    i.
+2.
+  a.
+    i.
+3.
+  a.
+    i.
+4.
+  a.
+    i.
+5.
+  a.
+    i.
+______________________________________________________________________________
+
+Now CODE with intent!
+=end
+
+require 'pry'
+require 'pry-byebug'
+
+# solution 1 - 
+
+def fibonacci_last(n)
+  penultimate = 1
+  ultimate = 1
+  return 1 if n <=2
+  
+  (n - 2).times do
+    sum = penultimate + ultimate
+    penultimate = ultimate
+    ultimate = sum
+  end
+  
+  string = ultimate.to_s
+  string[-1].to_i
+end
+
+# -------------------
+
+# solution 2 - 
+
+# -------------------
+
+# solution 3 - 
+
+# -------------------
+
+# solution 4 - 
+
+# -------------------
+
+# solution 5 - 
+
+# -------------------
+
+# Print Tests
+# p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+# p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+# p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+# p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+# p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+p fibonacci_last(123456789) # -> 4
+
+# Test Cases
