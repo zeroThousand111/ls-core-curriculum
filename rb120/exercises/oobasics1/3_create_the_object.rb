4e8c2efe60a9925c17d58c1ae2f8e481d7b58bdc
Graham Jarvis <zeroThousand111@proton.me>
2024-09-16 14:14:34 +0000

    3_create_the_object.rb RB120

---
+++ rb120/exercises/oobasics1/3_create_the_object.rb
@@ -0,0 +1,13 @@
+=begin
+Create the Object
+Using the code from the previous exercise, create an instance of Cat and assign it to a variable named kitty.
+
+Code:
+class Cat
+end
+=end
+
+class Cat
+end
+
+kitty = Cat.new
