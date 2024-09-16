fafd98f782a5126aecb26e0c441eb1410410f8ff
Graham Jarvis <zeroThousand111@proton.me>
2024-09-16 14:37:25 +0000

    9_accessor.rbRB120

---
+++ rb120/exercises/oobasics1/9_accessor.rb
@@ -0,0 +1,46 @@
+=begin
+Accessor
+Using the code from the previous exercise, replace the getter and setter methods using Ruby shorthand.
+
+Code:
+
+class Cat
+  attr_reader :name
+  attr_writer :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello! My name is #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+kitty.greet
+kitty.name = 'Luna'
+kitty.greet
+
+Expected output:
+
+Hello! My name is Sophie!
+Hello! My name is Luna!
+=end
+
+class Cat
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello! My name is #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+kitty.greet
+kitty.name = 'Luna'
+kitty.greet
