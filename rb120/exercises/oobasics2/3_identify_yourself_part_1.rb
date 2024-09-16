a815cf6e3963cb857e92f123a785a3434537e5b8
Graham Jarvis <zeroThousand111@proton.me>
2024-09-16 19:41:52 +0000

    3_identify_yourself_part_1.rb RB120

---
+++ rb120/exercises/oobasics2/3_identify_yourself_part_1.rb
@@ -0,0 +1,34 @@
+=begin
+Using the following code, add a method named #identify that returns its calling object.
+
+class Cat
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+kitty = Cat.new('Sophie')
+p kitty.identify
+
+Expected output (yours may contain a different object id):
+
+#<Cat:0x007ffcea0661b8 @name="Sophie">
+end
+=end
+
+class Cat
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+  
+  def identify
+    self
+  end
+end
+
+kitty = Cat.new('Sophie')
+p kitty.identify
