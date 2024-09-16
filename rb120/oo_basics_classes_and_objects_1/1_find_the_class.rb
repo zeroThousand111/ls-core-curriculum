105d4dbbc2ccec36ca66a537c074150afe955116
Graham Jarvis <zeroThousand111@proton.me>
2024-09-16 08:05:50 +0000

    1_find_the_class.rb for oo basics RB120

---
+++ rb120/oo_basics_classes_and_objects_1/1_find_the_class.rb
@@ -0,0 +1,17 @@
+=begin
+Update the following code so that, instead of printing the values, each statement prints the name of the class to which it belongs.
+
+puts "Hello"
+puts 5
+puts [1, 2, 3]
+
+Expected output:
+
+String
+Integer
+Array
+=end
+
+puts "Hello".class
+puts 5.class
+puts [1, 2, 3].class
