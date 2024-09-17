ed3a145768db10af66778ee7d5b8c0d7c1cf886e
Graham Jarvis <zeroThousand111@proton.me>
2024-09-17 19:54:07 +0000

    7_method_lookup_part_1.rb RB120

---
+++ rb120/exercises/inheritance/7_method_lookup_part_1.rb
@@ -0,0 +1,41 @@
+=begin
+Method Lookup (Part 1)
+Using the following code, determine the lookup path used when invoking cat1.color. Only list the classes that were checked by Ruby when searching for the #color method.
+
+class Animal
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+end
+
+cat1 = Cat.new('Black')
+cat1.color
+=end
+
+class Animal
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+end
+
+cat1 = Cat.new('Black')
+cat1.color
+
+puts Cat.ancestors
+p Cat.ancestors
