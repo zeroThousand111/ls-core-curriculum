97ff00b393bd244657887c1cdfab5518d27377ea
Graham Jarvis <zeroThousand111@proton.me>
2024-09-21 08:32:18 +0000

    4_fix_the_program_books_part_2.rb RB120

---
+++ rb120/exercises/easy1/4_fix_the_program_books_part_2.rb
@@ -0,0 +1,34 @@
+=begin
+Complete this program so that it produces the expected output:
+
+class Book
+  def to_s
+    %("#{title}", by #{author})
+  end
+end
+
+book = Book.new
+book.author = "Neil Stephenson"
+book.title = "Snow Crash"
+puts %(The author of "#{book.title}" is #{book.author}.)
+puts %(book = #{book}.)
+
+Expected output:
+
+The author of "Snow Crash" is Neil Stephenson.
+book = "Snow Crash", by Neil Stephenson.
+=end
+
+class Book
+  attr_accessor :title, :author
+  
+  def to_s
+    %("#{title}", by #{author})
+  end
+end
+
+book = Book.new
+book.author = "Neil Stephenson"
+book.title = "Snow Crash"
+puts %(The author of "#{book.title}" is #{book.author}.)
+puts %(book = #{book}.)
