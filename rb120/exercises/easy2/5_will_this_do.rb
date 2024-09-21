a7cd24cad15c7b9ea5bc75fb9a1e84f259492ff0
Graham Jarvis <zeroThousand111@proton.me>
2024-09-21 19:35:27 +0000

    5_will_this_do.rb RB120

---
+++ rb120/exercises/easy2/5_will_this_do.rb
@@ -0,0 +1,40 @@
+=begin
+What will the following code print?
+
+class Something
+  def initialize
+    @data = 'Hello'
+  end
+
+  def dupdata
+    @data + @data
+  end
+
+  def self.dupdata
+    'ByeBye'
+  end
+end
+
+thing = Something.new
+puts Something.dupdata
+puts thing.dupdata
+
+=end
+
+class Something
+  def initialize
+    @data = 'Hello'
+  end
+
+  def dupdata
+    @data + @data
+  end
+
+  def self.dupdata
+    'ByeBye'
+  end
+end
+
+thing = Something.new
+puts Something.dupdata  # => ByeBye
+puts thing.dupdata      # => HelloHello
