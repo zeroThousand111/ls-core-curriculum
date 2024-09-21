24fc1420de0837a09988ef9e80658bcd6756fe54
Graham Jarvis <zeroThousand111@proton.me>
2024-09-21 13:52:55 +0000

    2_fix_the_program_drivable.rb RB120

---
+++ rb120/exercises/easy2/2_fix_the_program_drivable.rb
@@ -0,0 +1,29 @@
+=begin
+Fix the Program - Drivable
+Correct the following program so it will work properly. Assume that the Car class has a complete implementation; just make the smallest possible change to ensure that cars have access to the drive method.
+
+module Drivable
+  def self.drive
+  end
+end
+
+class Car
+  include Drivable
+end
+
+bobs_car = Car.new
+bobs_car.drive
+=end
+
+module Drivable
+  def drive
+  end
+end
+
+class Car
+  include Drivable
+end
+
+bobs_car = Car.new
+bobs_car.drive
+
