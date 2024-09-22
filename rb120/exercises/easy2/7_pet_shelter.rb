819f1a16d8c19a6b180f116267f35eac768b1bae
Graham Jarvis <zeroThousand111@proton.me>
2024-09-22 07:18:55 +0000

    7_pet_shelter.rb further exploration RB120

--- rb120/exercises/easy2/7_pet_shelter.rb
+++ rb120/exercises/easy2/7_pet_shelter.rb
@@ -105,8 +105,17 @@ sweetie      = Pet.new('parakeet', 'Sweetie Pie')
 molly        = Pet.new('dog', 'Molly')
 chester      = Pet.new('fish', 'Chester')
 
+asta = Pet.new('dog', 'Asta')
+laddie = Pet.new('dog', 'Laddie')
+fluffy = Pet.new('cat', 'Fluffy')
+kat = Pet.new('cat', 'Kat')
+ben = Pet.new('cat', 'Ben')
+chatterbox = Pet.new('parakeet', 'Chatterbox')
+bluebell = Pet.new('parakeet', 'Bluebell')
+
 phanson = Owner.new('P Hanson')
 bholmes = Owner.new('B Holmes')
+pet_shelter = Owner.new('Pet Shelter')
 
 shelter = Shelter.new
 shelter.adopt(phanson, butterscotch)
@@ -116,6 +125,38 @@ shelter.adopt(bholmes, kennedy)
 shelter.adopt(bholmes, sweetie)
 shelter.adopt(bholmes, molly)
 shelter.adopt(bholmes, chester)
+
+shelter.adopt(pet_shelter, asta)
+shelter.adopt(pet_shelter, laddie)
+shelter.adopt(pet_shelter, fluffy)
+shelter.adopt(pet_shelter, kat)
+shelter.adopt(pet_shelter, ben)
+shelter.adopt(pet_shelter, chatterbox)
+shelter.adopt(pet_shelter, bluebell)
+
 shelter.print_adoptions
 puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
 puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
+puts "#{pet_shelter.name} has #{pet_shelter.number_of_pets} adopted pets."
+
+=begin
+Further Exploration
+Add your own name and pets to this project.
+
+Suppose the shelter has a number of not-yet-adopted pets, and wants to manage them through this same system. Thus, you should be able to add the following output to the example output shown above:
+
+The Animal Shelter has the following unadopted pets:
+a dog named Asta
+a dog named Laddie
+a cat named Fluffy
+a cat named Kat
+a cat named Ben
+a parakeet named Chatterbox
+a parakeet named Bluebell
+   ...
+
+P Hanson has 3 adopted pets.
+B Holmes has 4 adopted pets.
+The Animal shelter has 7 unadopted pets.
+Can you make these updates to your solution? Did you need to change your class system at all? Were you able to make all of your changes without modifying the existing interface?
+=end
