a7bf4c2813a2860bc881c604199b40151f5142cf
Graham Jarvis <zeroThousand111@proton.me>
2024-09-24 16:39:41 +0000

    fighting_fantasy.rb create Game class for code orchestration

--- rb120/fighting_fantasy.rb
+++ rb120/fighting_fantasy.rb
@@ -1,22 +1,35 @@
 =begin
+## FUNCTIONS
 A fighting fantasy tool:
 1. A character generator
 2. A monster generator
 3. A fighting model
 
+## CHARACTER GENERATOR
+
+## MONSTER GENERATOR
+
+## FIGHT MODEL
+
+## HEIRARCHY
+
+AliveObject
+  - Player
+  - Monster
+    - Goblin
+
+Game
+  - Fight
+
 =end
 
 class AliveObject
   # include Comparable
-
   attr_reader :skill, :max_stamina, :name
   attr_accessor :current_stamina
 
   def initialize(name)
     @name = name
-    @skill = rand(6) + 4
-    @max_stamina = rand(12) + 4
-    @current_stamina = @max_stamina
   end
 
   def to_s
@@ -28,8 +41,8 @@ class AliveObject
   # end
 
   def fight(other)
-    my_score = skill + rand(12)
-    other_score = other.skill + rand(12)
+    my_score = skill + rand(1..12)
+    other_score = other.skill + rand(1..12)
 
     puts "#{name}'s score is #{my_score}"
     puts "#{other.name}'s score is #{other_score}"
@@ -39,25 +52,22 @@ class AliveObject
       other.current_stamina -= 2
     elsif other_score > my_score
       puts "#{other.name} wins the round!"
-      current_stamina -= 2
+      self.current_stamina -= 2
     else
       puts "this round is a draw!"
     end
-
   end
-
 end
 
 class Player < AliveObject
-
   attr_reader :luck
 
   def initialize(name)
-    @name = name
-    @skill = rand(6) + 6
-    @max_stamina = rand(12) + 12
+    super
+    @skill = rand(7..12)
+    @max_stamina = rand(13..24)
     @current_stamina = @max_stamina
-    @luck = rand(6) + 6
+    @luck = rand(7..12)
   end
 
   def to_s
@@ -66,11 +76,25 @@ class Player < AliveObject
 end
 
 class Monster < AliveObject
+  def initialize(name)
+    super
+    @skill = rand(5..10)
+    @max_stamina = rand(5..16)
+    @current_stamina = @max_stamina
+  end
 end
 
-goblin1 = Monster.new("goblin")
-hero = Player.new("conan")
+class Game
+  def play
+    monster = Monster.new("goblin")
+    hero = Player.new("conan")
+    puts monster, hero
+    loop do
+      hero.fight(monster)
+      break if monster.current_stamina == 0 || hero.current_stamina == 0
+    end
+    puts monster, hero
+  end
+end
 
-puts goblin1, hero
-hero.fight(goblin1)
-puts goblin1, hero
+Game.new.play
