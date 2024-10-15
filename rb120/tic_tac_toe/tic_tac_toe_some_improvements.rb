96ed037a2458b72306e96f280f0537afcf25bbe5
Graham Jarvis <zeroThousand111@proton.me>
2024-10-15 17:33:23 +0100

    tic_tac_toe_some_improvements.rb refactor get_square_at to getter method RB120

--- rb120/tic_tac_toe/tic_tac_toe_some_improvements.rb
+++ rb120/tic_tac_toe/tic_tac_toe_some_improvements.rb
@@ -13,9 +13,9 @@ class Board
   def get_square_at(key)
 		@squares[key]
   end
-  
-  def set_square_at(key, marker)
-    @squares[key].marker = marker
+
+  def []=(num, marker)
+    @squares[num].marker = marker
   end
   
   def unmarked_keys
@@ -138,11 +138,11 @@ class TTTGame
       puts "Sorry, that's not a valid choice."
     end
 
-    board.set_square_at(square, human.marker)
+    board[square] = human.marker
   end
   
   def computer_moves
-    board.set_square_at(board.unmarked_keys.sample, computer.marker)
+    board[board.unmarked_keys.sample] = computer.marker
   end
     
   def display_result
