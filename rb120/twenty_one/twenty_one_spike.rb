9d2b2c80b277e2a28f7491196d9cd9f3e88fcf4e
Graham Jarvis <zerothousand111@proton.me>
2024-10-20 10:51:21 +0100

    twenty_one_spike.rb add new Participant instance variables RB120

--- rb120/twenty_one/twenty_one_spike.rb
+++ rb120/twenty_one/twenty_one_spike.rb
@@ -11,32 +11,36 @@ Here is an overview of the game:
 =end
 
 class Participant
-    # attr_reader :type_of_player
-    attr_accessor   :hand
-
-    # what goes in here? all the redundant behaviors from Player and Dealer?
+    attr_accessor   :hand, :total, :busted
 
     def initialize #(type_of_player)
-        # what would the "data" or "states" of a Player object entail?
-        # maybe cards? a name?
         @hand = []
-        # @type_of_player = type_of_player
+        @total = 0
+        @busted = false
     end
 
-    def display_cards
-        puts "Your hand is:"
-        hand.each do |card, suit, _|
-            puts "#{card.upcase} of #{suit.capitalize}"
+    def calculate_total
+        value_of_hand = 0
+        hand.each do |_, _, face_value|
+          value_of_hand += face_value
         end
-        puts ""
-        puts "The total value of your cards is #{total}"
+        @total = adjust_total_for_aces(value_of_hand)
+        change_busted_status?
     end
-  
-    def busted? # factor in aces. how to change value of ace cards if total > 21?
-        adjusted_total = total
-        aces_in_hand = count_aces_in_hand
-        adjusted_total -= 10 * aces_in_hand
-        adjusted_total > 21
+
+    private
+
+    def change_busted_status?
+        @busted = true if @total > 21
+    end
+
+    def adjust_total_for_aces(value_of_hand)
+        if @total > 21
+            aces_in_hand = count_aces_in_hand
+            return value_of_hand - (10 * aces_in_hand)
+        else
+            return value_of_hand
+        end
     end
 
     def count_aces_in_hand
@@ -48,100 +52,45 @@ class Participant
         end
         count
     end
+end
 
-    def display_busted
-        puts "Your total is #{total} and so you are BUST!"
-    end
-
-    def stay
-        puts "You have decided to stay."
-        display_cards
-    end
-  
-    def total
-      total = 0
-      hand.each do |_, _, value|
-        total += value
-      end
-      total
+class Player < Participant
+    def initialize
+      super
     end
 end
 
-# class Player < Participant - probably don't need separate class for player
-#     def initialize
-#       # what would the "data" or "states" of a Player object entail?
-#       # maybe cards? a name?
-#     end
-# end
+class Dealer < Participant
+    def initialize
+      super
+    end
 
-# class Dealer < Participant - probably don't need separate class for dealer
-#     def initialize
-#       # seems like very similar to Player... do we even need this?
-#     end
-# end
+    def display_one_card
+        "The dealer has the #{hand[0][0].upcase} of #{hand[0][1].upcase}, and an unknown card."
+    end
+end
 
 class Deck
     attr_reader :deck
-
-    # limited deck to test spike at first
-    # DECK_OF_CARDS = [
-    #     ['2S', 2], ['3S', 3], ['4S', 4], ['5S', 5], ['6S', 6], ['7S', 7], ['8S', 8], ['9S', 9], ['10S', 10], ['JofS', 10], ['QofS', 10], ['KofS', 10], ['AofS', 11], ['2D', 2], ['3D', 3], ['4D', 4], ['5D', 5], ['6D', 6], ['7D', 7], ['8D', 8], ['9D', 9], ['10D', 10], ['JofD', 10], ['QofD', 10], ['KofD', 10], ['AofD', 11]
-    # ]
 
     DECK_OF_CARDS = [
         ["two", "spades", 2], ["three", "spades", 3], ["four", "spades", 4], ["five", "spades", 5], ["six", "spades", 6], ["seven", "spades", 7], ["eight", "spades", 8], ["nine", "spades", 9], ["ten", "spades", 10], ["jack", "spades", 10], ["queen", "spades", 10], ["king", "spades", 10], ["ace", "spades", 11], ["two", "diamonds", 2], ["three", "diamonds", 3], ["four", "diamonds", 4], ["five", "diamonds", 5], ["six", "diamonds", 6], ["seven", "diamonds", 7], ["eight", "diamonds", 8], ["nine", "diamonds", 9], ["ten", "diamonds", 10], ["jack", "diamonds", 10], ["queen", "diamonds", 10], ["king", "diamonds", 10], ["ace", "diamonds", 11], ["two", "hearts", 2], ["three", "hearts", 3], ["four", "hearts", 4], ["five", "hearts", 5], ["six", "hearts", 6], ["seven", "hearts", 7], ["eight", "hearts", 8], ["nine", "hearts", 9], ["ten", "hearts", 10], ["jack", "hearts", 10], ["queen", "hearts", 10], ["king", "hearts", 10], ["ace", "hearts", 11], ["two", "spades", 2], ["three", "spades", 3], ["four", "spades", 4], ["five", "spades", 5], ["six", "spades", 6], ["seven", "spades", 7], ["eight", "spades", 8], ["nine", "spades", 9], ["ten", "spades", 10], ["jack", "spades", 10], ["queen", "spades", 10], ["king", "spades", 10], ["ace", "spades", 11]
     ]
 
     def initialize
-      # obviously, we need some data structure to keep track of cards - use an array of 'cards'
       @deck = create_deck
-      # array, hash, something else?
-      # hashes () {[name]=>face_value]}
-      # name
     end
   
     def deal
-      # does the dealer or the deck deal? - the deck to avoid confusion with the dealer also being a player
-      # deal must mutate deck object by removing cards from deck when they are added to either hand (so cards can't be used twice)
         deck.shift
     end
 
     private
 
     def create_deck
-        # need to be able to create and shuffle deck
-
-        # implementation where each card is an array ['name', value]
         deck = DECK_OF_CARDS.shuffle
-
-        # implementation where each card is a new Card object
-        # deck = []
-        # DECK_OF_CARDS.each do |card|
-        #     deck << Card.new(card[0], card[1])
-        # end
-        # deck.shuffle
-        # p deck
     end
 end
-
-# class Card
-#     def initialize(name, face_value)
-#       @name = name
-#       @face_value = face_value
-#       # or alt
-#       # @suit e.g. 'diamonds'
-#       # @name e.g. 'two'
-#       # @face_value
-#     end
-
-#     def card_name
-#         "#{name.upcase} of #{suit.capitalize}"
-#     end
-
-#     # def to_s
-#     #     "#{name.upcase} of #{suit.capitalize}"
-#     # end
-# end
   
 class Game
     attr_reader :deck, :player, :dealer
@@ -149,9 +98,9 @@ class Game
         # create randomised deck
         @deck = Deck.new
         # create player object
-        @player = Participant.new #("player")
+        @player = Player.new #("player")
         # create dealer object
-        @dealer = Participant.new #("dealer")
+        @dealer = Dealer.new #("dealer")
     end
 
     def start
@@ -164,34 +113,27 @@ class Game
  
     private
 
-    def deal_cards
-        player.hand << deck.deal << deck.deal
-        dealer.hand << deck.deal << deck.deal
-    end
-
-    def show_initial_cards
-        puts "You have been dealt two cards from the deck."
-        player.display_cards
-    end
+    # game methods
 
     def player_turn
         loop do
             choice = get_player_choice
             if choice == 'h'
                 player_hit
-                player.display_cards
-            elsif choice == 's' # is it necessary to even check?
-                player.stay # is this necessary to have a method doing nothing?
+                display_player_hand
+            elsif choice == 's'
+                display_stay
             end
-            break if choice == 's'  || player.busted?
+            break if choice == 's'  || player.busted
         end
-        player.display_busted if player.busted?
+        display_busted if player.busted
     end
 
     def get_player_choice
         valid_choices = ['h', 's']
         choice = nil
         loop do
+            puts ""
             puts "Would you like to hit or stay?  Press h or s"
             choice = gets.chomp.downcase
             break if valid_choices.include?(choice)
@@ -200,10 +142,64 @@ class Game
         choice
     end
 
+    # deck methods
+
+    def deal_cards
+        player.hand << deck.deal << deck.deal
+        dealer.hand << deck.deal << deck.deal
+    end
+
     def player_hit
         player.hand << deck.deal
     end
 
+    # display methods
+
+    def show_initial_cards
+        clear_screen
+        puts "You have been dealt two cards from the deck."
+        player.calculate_total
+        display_player_hand
+        puts ""
+        display_dealer_hand
+    end
+
+    def display_player_hand
+        puts ""
+        puts "Your hand is:"
+        puts ""
+        display_all_cards
+        puts ""
+        player.calculate_total
+        puts display_total
+    end
+
+    def display_all_cards
+        player.hand.each do |card, suit, _|
+            puts "#{card.upcase} of #{suit.capitalize}"
+        end
+    end
+
+    def display_dealer_hand
+        puts dealer.display_one_card
+    end
+
+    def display_total
+        "The total value of your cards is #{player.total}"
+    end
+
+    def display_busted
+        puts "Your total is #{player.total} and so you are BUST!"
+    end
+
+    def display_stay
+        puts "You have decided to stay."
+        display_player_hand
+    end
+
+    def clear_screen
+        system "clear"
+    end
 end
   
 Game.new.start
