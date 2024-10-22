905df9c747873f35f3795de580956722818c4795
Graham Jarvis <zerothousand111@proton.me>
2024-10-22 14:29:23 +0100

    files.rb in debugging exercises RB120

---
+++ rb120/exercises/debugging/files.rb
@@ -0,0 +1,59 @@
+=begin
+You started writing a very basic class for handling files. However, when you begin to write some simple test code, you get a NameError. The error message complains of an uninitialized constant File::FORMAT.
+
+What is the problem and what are possible ways to fix it?
+=end
+
+class File
+  attr_accessor :name, :byte_content
+
+  def initialize(name)
+    @name = name
+  end
+
+  alias_method :read,  :byte_content
+  alias_method :write, :byte_content=
+
+  def copy(target_file_name)
+    target_file = self.class.new(target_file_name)
+    target_file.write(read)
+
+    target_file
+  end
+
+  def to_s
+    "#{name}.#{self.class::FORMAT}"
+  end
+end
+
+class MarkdownFile < File
+  FORMAT = :md
+end
+
+class VectorGraphicsFile < File
+  FORMAT = :svg
+end
+
+class MP3File < File
+  FORMAT = :mp3
+end
+
+# Test
+
+blog_post = MarkdownFile.new('Adventures_in_OOP_Land')
+blog_post.write('Content will be added soon!'.bytes)
+
+copy_of_blog_post = blog_post.copy('Same_Adventures_in_OOP_Land')
+
+puts copy_of_blog_post.is_a? MarkdownFile     # true
+puts copy_of_blog_post.read == blog_post.read # true
+
+puts blog_post
+
+=begin
+The issue is that in the `File#to_s` instance method, it references the constant FORMAT, but this constant is defined three times in the classes `MarkdownFile`, `VectorGraphicsFile` and `MP3File`.  Which class should it reference?  The class the object belongs to, of course.  How to acheive this?  I reformatted `to_s` with this expression:
+
+"#{name}.#{self.class::FORMAT}"
+
+This is the same format used in the `File#copy` method above too.
+=end
