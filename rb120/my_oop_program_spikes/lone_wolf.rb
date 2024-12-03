378d854f6d2f0f79ffcd9bc0ee5a0d3e592ebf17
Graham Jarvis <zeroThousand111@proton.me>
2024-12-03 16:34:34 +0000

    evolve lone_wolf.rb

--- rb120/my_oop_program_spikes/lone_wolf.rb
+++ rb120/my_oop_program_spikes/lone_wolf.rb
@@ -1,24 +1,64 @@
 class AnimateObject
-  attr_reader :combat_skill, :endurance
+  attr_reader :name, :combat_skill, :endurance
 
-  def initialize(combat_skill, endurance)
-    @combat_skill = combat_skill
-    @endurance = endurance
+  def initialize
+    @name = choose_name
+    @combat_skill = choose_skill
+    @endurance = choose_endurance
+  end
+
+  def choose_name
+    name_choice = nil
+    loop do 
+      puts "Please write the #{self.class} name:"
+      name_choice = gets.chomp
+      puts "You've chosen #{name_choice.upcase} as the name of the #{self.class}, is that correct? (Y/N)"
+      validation = gets.chomp
+      break if validation.downcase == "y"
+    end
+    name_choice
   end
 
   def display_stats
     puts "REMINDER: you need to write a `display_stats` method for this class!"
   end
+
+  def choose_skill
+    skill = nil
+    loop do
+      puts "Please enter the endurance of #{self.class}:"
+      skill = gets.chomp
+      break if valid_stat?(skill)
+      puts "Sorry, that input isn't valid."
+    end
+    skill.to_i
+  end
+
+  def choose_endurance
+    endurance = nil
+    loop do
+      puts "Please enter the endurance of #{self.class}:"
+      endurance = gets.chomp
+      break if valid_stat?(endurance)
+      puts "Sorry, that input isn't valid."
+    end
+    endurance.to_i
+  end
+
+  private
+
+  def valid_stat?(entry)
+    entry.chars.all? do |char|
+      (0..9).to_a.map(&:to_s).include?(char)
+    end
+  end
 end
 
 class Player < AnimateObject
-  attr_reader :name
-
   def initialize
     valid_choices = ["y", "n"]
 
     stats_already = nil
-  
     loop do
       puts "Do you already have stats for your player? (Y/N)"
       stats_already = gets.chomp
@@ -27,10 +67,11 @@ class Player < AnimateObject
     end
 
     if stats_already == "y"
-      chosen_skill = enter_skill
-      chosen_endurance = enter_endurance
-      super(chosen_skill, chosen_endurance)
-      @name = choose_name
+      super
+      # chosen_skill = choose_skill
+      # chosen_endurance = choose_endurance
+      # super(chosen_skill, chosen_endurance)
+      # @name = choose_name
     else
       super(randomise_skill, randomise_endurance)
       @name = choose_name
@@ -46,28 +87,6 @@ class Player < AnimateObject
 
   private
 
-  def choose_name
-    name_choice = nil
-    loop do 
-      puts "Please write your name:"
-      name_choice = gets.chomp
-      puts "You've chosen #{name_choice} as your name, is that correct? (Y/N)"
-      validation = gets.chomp
-      break if validation.downcase == "y"
-    end
-    name_choice
-  end
-
-  def enter_skill
-    puts "Please enter your combat skill:"
-    entry = gets.chomp
-  end
-
-  def enter_endurance
-    puts "Please enter your endurance:"
-    entry = gets.chomp
-  end
-
   def randomise_skill
     rand(0..9) + 10
   end
@@ -78,11 +97,8 @@ class Player < AnimateObject
 end
 
 class Monster < AnimateObject
-  attr_reader :name
-
-  def initialize(name, combat_skill, endurance)
-    super(combat_skill, endurance)
-    @name = name
+  def initialize
+    super
   end
 
   def display_stats
@@ -95,16 +111,13 @@ end
 
 class Fight
   def initialize
-    monster = Monster.new("helghast", 10, 20)
+    monster = Monster.new
     lone_wolf = Player.new
 
     monster.display_stats
     lone_wolf.display_stats
   end
 end
-
-# helghast = Monster.new("helghast", 10, 20)
-# lone_wolf = Player.new
 
 Fight.new
 
