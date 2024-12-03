ea18d96bac76bfcddb43fc117546f34add565b28
Graham Jarvis <zerothousand111@proton.me>
2024-12-03 21:07:38 +0000

    create fight system for lone_wolf.rb

--- rb120/my_oop_program_spikes/lone_wolf.rb
+++ rb120/my_oop_program_spikes/lone_wolf.rb
@@ -1,5 +1,6 @@
 class AnimateObject
-  attr_reader :name, :combat_skill, :endurance
+  attr_reader :name, :combat_skill
+  attr_accessor :endurance
 
   def initialize
     @name = choose_name
@@ -12,21 +13,29 @@ class AnimateObject
     loop do 
       puts "Please write the #{self.class} name:"
       name_choice = gets.chomp
-      puts "You've chosen #{name_choice.upcase} as the name of the #{self.class}, is that correct? (Y/N)"
-      validation = gets.chomp
-      break if validation.downcase == "y"
+      # puts "You've chosen #{name_choice.upcase} as the name of the #{self.class}, is that correct? (Y/N)"
+      # validation = gets.chomp
+      # break if validation.downcase == "y"
+      break
     end
     name_choice
   end
 
   def display_stats
-    puts "REMINDER: you need to write a `display_stats` method for this class!"
+    puts ""
+    puts "Name: #{name.upcase}."
+    puts "Combat Skill: #{combat_skill}."
+    puts "Endurance: #{endurance}."
+  end
+
+  def display_endurance
+    puts "Current endurance of #{name.upcase}: #{endurance}"
   end
 
   def choose_skill
     skill = nil
     loop do
-      puts "Please enter the endurance of #{self.class}:"
+      puts "Please enter the COMBAT SKILL of #{self.class}:"
       skill = gets.chomp
       break if valid_stat?(skill)
       puts "Sorry, that input isn't valid."
@@ -37,7 +46,7 @@ class AnimateObject
   def choose_endurance
     endurance = nil
     loop do
-      puts "Please enter the endurance of #{self.class}:"
+      puts "Please enter the ENDURANCE of #{self.class}:"
       endurance = gets.chomp
       break if valid_stat?(endurance)
       puts "Sorry, that input isn't valid."
@@ -68,21 +77,11 @@ class Player < AnimateObject
 
     if stats_already == "y"
       super
-      # chosen_skill = choose_skill
-      # chosen_endurance = choose_endurance
-      # super(chosen_skill, chosen_endurance)
-      # @name = choose_name
     else
-      super(randomise_skill, randomise_endurance)
       @name = choose_name
+      @combat_skill = randomise_skill
+      @endurance = randomise_endurance
     end
-  end
-
-  def display_stats
-    puts ""
-    puts "My name is #{name.upcase}."
-    puts "My combat skill is : #{combat_skill}."
-    puts "My endurance is : #{endurance}."
   end
 
   private
@@ -100,24 +99,151 @@ class Monster < AnimateObject
   def initialize
     super
   end
-
-  def display_stats
-    puts ""
-    puts "I am a #{name.upcase}."
-    puts "My combat skill is : #{combat_skill}."
-    puts "My endurance is : #{endurance}."
-  end
 end
 
 class Fight
-  def initialize
-    monster = Monster.new
-    lone_wolf = Player.new
+  attr_reader :combat_ratio
 
+  @@rounds = 0
+
+  def initialize # the orchestration method
+    monster = Monster.new
+    player = Player.new
+    display_all_stats(monster, player)
+    @combat_ratio = player.combat_skill - monster.combat_skill
+    display_combat_ratio
+    loop do
+      one_round(monster, player)
+      display_endurances_both(monster, player)
+      # break
+      break if monster.endurance <= 0 || player.endurance <= 0
+    end
+    determine_winner(monster, player)
+  end
+
+  def display_all_stats(monster, player)
     monster.display_stats
-    lone_wolf.display_stats
+    player.display_stats
+  end
+
+  def display_endurances_both(monster, player)
+    monster.display_endurance
+    player.display_endurance
+  end
+
+  def display_combat_ratio
+    puts ""
+    puts "Combat ratio is: #{combat_ratio}."
+  end
+
+  def one_round(monster, player)
+    @@rounds += 1
+    random_number = rand(0..9)
+    puts ""
+    puts "The random number is: #{random_number}"
+    monster.endurance -= calculate_monster_damage(random_number)
+    player.endurance -= calculate_player_damage(random_number)
+  end
+
+  def determine_winner(monster, player)
+    puts ""
+    puts "After #{@@rounds} rounds..."
+    display_endurances_both(monster, player)
+    if monster.endurance <= 0 && player.endurance > 0 # monster dies
+      puts "#{player.name.upcase}, you are victorious!"
+    elsif monster.endurance > 0 && player.endurance <= 0 # player dies
+      puts "Unfortunately, #{player.name.upcase}, your quest ends here." 
+    elsif monster.endurance <= 0 && player.endurance <=0 # both die
+      puts "Well that's a turn up!"
+      puts "Both #{player.name.upcase} and #{monster.name.upcase} have died!"
+    end
+  end
+
+  private
+
+  # damage calculations only present for combat ratio of 8
+
+  def calculate_monster_damage(random_number)
+    damage = 0
+    case combat_ratio
+    when -10..-9
+    when -8..-7
+    when -6..-5
+    when -4..-3
+    when -2..-1
+    when 0
+    when 1..2
+    when 3..4
+    when 5..6
+    when 7..8
+      case random_number
+      when 1
+        damage = 7
+      when 2
+        damage = 8
+      when 3
+        damage = 9
+      when 4
+        damage = 10
+      when 5
+        damage = 11
+      when 6
+        damage = 12
+      when 7
+        damage = 14
+      when 8
+        damage = 16
+      when 9
+        damage = 18
+      when 0
+        damage = 18
+      end
+    when 9..10
+    when 11..100
+    end
+    damage
+  end
+
+  def calculate_player_damage(random_number)
+    damage = 0
+    case combat_ratio
+    when -10..-9
+    when -8..-7
+    when -6..-5
+    when -4..-3
+    when -2..-1
+    when 0
+    when 1..2
+    when 3..4
+    when 5..6
+    when 7..8
+      case random_number
+      when 1
+        damage = 4
+      when 2
+        damage = 3
+      when 3
+        damage = 2
+      when 4
+        damage = 2
+      when 5
+        damage = 2
+      when 6
+        damage = 1
+      when 7
+        damage = 0
+      when 8
+        damage = 0
+      when 9
+        damage = 0
+      when 0
+        damage = 0
+      end
+    when 9..10
+    when 11..100
+    end
+    damage
   end
 end
 
 Fight.new
-
