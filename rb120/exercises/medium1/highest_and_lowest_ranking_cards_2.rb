1a75292d2abe8bb87fbce3004bdb300d19d58b27
Graham Jarvis <zerothousand111@proton.me>
2025-01-21 12:55:48 +0000

    highest_and_lowest_ranking_cards_2.rb for medium1 exercises RB120

---
+++ rb120/exercises/medium1/highest_and_lowest_ranking_cards_2.rb
@@ -0,0 +1,88 @@
+# Update this class so you can use it to determine the lowest ranking and highest ranking cards in an Array of Card objects:
+
+class Card
+  include Comparable
+  attr_reader :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  protected
+  def <=>(other)
+    self.interpret_rank <=> other.interpret_rank
+  end
+
+  def interpret_rank
+    case rank
+    when 2..10 then rank
+    when 'Jack' then 11
+    when 'Queen' then 12
+    when 'King' then 13
+    when 'Ace' then 14
+    end
+  end
+end
+
+=begin
+For this exercise, numeric cards are low cards, ordered from 2 through 10. Jacks are higher than 10s, Queens are higher than Jacks, Kings are higher than Queens, and Aces are higher than Kings. The suit plays no part in the relative ranking of cards.
+
+If you have two or more cards of the same rank in your list, your min and max methods should return one of the matching cards; it doesn't matter which one.
+
+Besides any methods needed to determine the lowest and highest cards, create a #to_s method that returns a String representation of the card, e.g., "Jack of Diamonds", "4 of Clubs", etc.
+
+Examples:
+=end
+
+cards = [Card.new(2, 'Hearts'),
+         Card.new(10, 'Diamonds'),
+         Card.new('Ace', 'Clubs')]
+puts cards
+puts cards.min == Card.new(2, 'Hearts')
+puts cards.max == Card.new('Ace', 'Clubs')
+
+cards = [Card.new(5, 'Hearts')]
+puts cards.min == Card.new(5, 'Hearts')
+puts cards.max == Card.new(5, 'Hearts')
+
+cards = [Card.new(4, 'Hearts'),
+         Card.new(4, 'Diamonds'),
+         Card.new(10, 'Clubs')]
+puts cards.min.rank == 4
+puts cards.max == Card.new(10, 'Clubs')
+
+cards = [Card.new(7, 'Diamonds'),
+         Card.new('Jack', 'Diamonds'),
+         Card.new('Jack', 'Spades')]
+puts cards.min == Card.new(7, 'Diamonds')
+puts cards.max.rank == 'Jack'
+
+cards = [Card.new(8, 'Diamonds'),
+         Card.new(8, 'Clubs'),
+         Card.new(8, 'Spades')]
+puts cards.min.rank == 8
+puts cards.max.rank == 8
+
+=begin
+Output:
+
+Copy Code
+2 of Hearts
+10 of Diamonds
+Ace of Clubs
+true
+true
+true
+true
+true
+true
+true
+true
+true
+true
+=end
