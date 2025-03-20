9dafaa2a0a252ec7e4354cffb4fc55eb577b9886
Graham Jarvis <zerothousand111@proton.me>
2025-03-20 19:29:18 +0000

    updated car.rb and car_test.rb for more assertion tests RB130 Lesson 2

--- rb130/lesson_2/car_test.rb
+++ rb130/lesson_2/car_test.rb
@@ -1,21 +1,61 @@
 # assertion syntax / assert-style syntax
 
 require 'minitest/autorun'
-require "minitest/reporters"
-Minitest::Reporters.use!
+# require "minitest/reporters"
+# Minitest::Reporters.use!
 
 require_relative 'car'
 
 class CarTest < Minitest::Test
+  # def test_wheels
+  #   car = Car.new
+  #   assert_equal(4, car.wheels)
+  # end
+
+  # def test_bad_wheels
+  #   # skip("I've skipped the bad wheels test")
+  #   car = Car.new
+  #   assert_equal(3, car.wheels)
+  # end
+
+  # assert
+  def test_car_exists
+    car = Car.new
+    assert(car)
+  end
+
+  # assert_equal
   def test_wheels
     car = Car.new
     assert_equal(4, car.wheels)
   end
 
-  def test_bad_wheels
-    skip("I've skipped the bad wheels test")
+  # assert_nil
+  def test_name_is_nil
     car = Car.new
-    assert_equal(3, car.wheels)
+    assert_nil(car.name)
+  end
+
+  # assert_raises
+  def test_raise_initialize_with_arg
+    assert_raises(ArgumentError) do
+      car = Car.new(name: "Joey")         # this code raises ArgumentError, so this assertion passes
+    end
+  end
+
+  # assert_instance_of
+  def test_instance_of_car
+    car = Car.new
+    assert_instance_of(Car, car)
+  end
+
+  # assert_includes
+  def test_includes_car
+    car = Car.new
+    arr = [1, 2, 3]
+    arr << car
+  
+    assert_includes(arr, car)
   end
 end
 
