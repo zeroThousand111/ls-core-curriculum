3dd1bf0a8ee6d919a65871dba22494b5c457d828
Graham Jarvis <zeroThousand111@proton.me>
2025-03-26 08:28:02 +0000

    finished text_analyzer_sandwich_code.rb in medium1 exercises RNB130

--- rb130/more_topics_exercises/medium1/text_analyzer_sandwich_code.rb
+++ rb130/more_topics_exercises/medium1/text_analyzer_sandwich_code.rb
@@ -66,6 +66,7 @@ class TextAnalyzer
 
   def process
     # your implementation
+    # initialise three count variables
     paragraphs = 1
     lines = 0
     words = 0
@@ -74,16 +75,23 @@ class TextAnalyzer
     # file name obtained when yielding 
     text = File.open(yield) { |file| file.read }
 
-    # yield to the block and count things
-    # split into paragraphs then count size
+    # split into array of paragraphs then count size
+    array_of_paragraphs = text.split(/\n\n/)
+    paragraphs = array_of_paragraphs.size
 
-    # split each paragraph into lines then count size
+    # split each paragraph into an array of lines then count size and increment value of lines
+    array_of_paragraphs.each do |paragraph|
+      array_of_lines = paragraph.split(/\n/)
+      lines += array_of_lines.size
 
-    # split each line into words, then count size
-
+      # split each line into an array of words, then count size and increment value of words
+      array_of_lines.each do |line|
+        array_of_words = line.split(" ")
+        words += array_of_words.size
+      end
+    end
 
     # output variables as a HEREDOC
-
     puts <<-OUTPUT
     #{paragraphs} paragraphs
     #{lines} lines
@@ -96,10 +104,51 @@ end
 
 analyzer = TextAnalyzer.new
 analyzer.process { 'sample_text.txt' }
-# analyzer.process { # your implementation }
+analyzer.process { 'more_sample_text.txt' }
 # analyzer.process { # your implementation }
 
 # sample output
 # 3 paragraphs
 # 15 lines
 # 126 words
+
+# LS Solution
+
+class TextAnalyzer
+  def process
+    file = File.open('sample_text.txt', 'r')
+    yield(file.read)
+    file.close
+  end
+end
+
+analyzer = TextAnalyzer.new
+analyzer.process { |text| puts "#{text.split("\n\n").count} paragraphs" }
+analyzer.process { |text| puts "#{text.split("\n").count} lines" }
+analyzer.process { |text| puts "#{text.split(' ').count} words" }
+
+=begin
+Discussion
+For this exercise, you must fill in the missing parts of the code we gave you. You know the output we want; you must somehow produce that output.
+
+Start with the #process method; we told you that #process should read some text from a file, and then pass that text to the block for further processing. The usual approach for such read-and-process code looks like this:
+
+file = File.open('sample_text.txt', 'r')
+# do processing here
+file.close
+Remember to always close files when you finish using them.
+
+Since we're supposed to pass the text content of the file to the block, we must load the file's content and call the block, which we do with yield(file.read):
+
+file = File.open('sample_text.txt', 'r')
+yield(file.read)
+file.close
+At this point we have what we call "sandwich code"; it performs some pre- and post-processing for some arbitrary action. Here, the pre-processing opens a file, and the post-processing closes it. Together, they sandwich the action that loads the file's contents and passes it to a block.
+
+Since we're passing text to the blocks, the blocks should capture that text:
+
+analyzer.process { |text| puts ... }
+analyzer.process { |text| puts ... }
+analyzer.process { |text| puts ... }
+Judging from the partial code, you can see that each block contains a puts statement, so we must provide arguments to puts that perform the necessary processing and format the answer accordingly. The last step fills in these details: for each, we use String#split and Array#count as you can see in the Solution.
+=end
