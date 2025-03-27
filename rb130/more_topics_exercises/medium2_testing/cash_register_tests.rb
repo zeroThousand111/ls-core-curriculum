737571704ce28b94f45bfe8a6c076692ca3e5e61
Graham Jarvis <zeroThousand111@proton.me>
2025-03-27 13:25:37 +0000

    cash_register_tests.rb tests for accept_money, change and give_receipt medium2 exercises RB130

--- rb130/more_topics_exercises/medium2_testing/cash_register_tests.rb
+++ rb130/more_topics_exercises/medium2_testing/cash_register_tests.rb
@@ -4,4 +4,42 @@ require_relative 'transaction'
 
 class CashRegisterTest < Minitest::Test
 
+  # def setup
+  # end
+
+  def test_accept_money
+    register = CashRegister.new(100)
+    transaction = Transaction.new(1)
+    transaction.amount_paid = 1
+
+    previous_total = register.total_money
+    register.accept_money(transaction)
+    current_total = register.total_money
+
+    assert_equal(previous_total + 1, current_total)
+  end
+  
+  def test_change
+    register = CashRegister.new(100)
+    transaction = Transaction.new(10)
+    transaction.amount_paid = 10
+
+    item_cost = transaction.item_cost
+    amount_paid = transaction.amount_paid
+    
+    assert_equal(amount_paid - item_cost, register.change(transaction))
+  end
+  
+  def test_give_receipt
+    item_cost = 10
+    register = CashRegister.new(100)
+    transaction = Transaction.new(item_cost)
+
+    assert_output("You've paid $#{item_cost}.\n") { register.give_receipt(transaction) }
+  end
+
+  
 end
+
+
+
