df5e1ed9825f4b5715cc847933651aebeefba00b
Graham Jarvis <zerothousand111@proton.me>
2025-04-25 12:22:47 +0100

    simple linked list second draft in medium challenges RB130

--- rb130/challenges/medium_challenges/simple_linked_list.rb
+++ rb130/challenges/medium_challenges/simple_linked_list.rb
@@ -1,4 +1,112 @@
 # SOLUTION 1 - FIRST DRAFT - WITHOUT HINTS JUST WITH TEST SUITE
+# class Element
+#   attr_reader :datum
+
+#   def initialize(datum, next_element=nil)
+#     @datum = datum
+#     @next_element = next_element
+#   end
+
+#   def next
+#     @next_element
+#   end
+
+#   def tail?
+#     self.next.nil?
+#   end
+
+#   # def ==(other) # my own method for testing
+#   #   self.datum == other.datum && self.next_element == other.next_element
+#   # end
+# end
+
+# class SimpleLinkedList
+#   include Enumerable
+
+#   attr_accessor :head
+
+#   def self.from_a(array)
+#     new_list = SimpleLinkedList.new
+#     return new_list if array.nil?
+
+#     array.reverse_each do |value|
+#       new_list.push(value)
+#     end
+#     new_list
+#   end
+
+#   def initialize
+#     @head = nil
+#   end
+
+#   def size
+#     return 0 if self.empty?
+#     self.count # using Enumerable#count
+#   end
+
+#   def empty?
+#     head.nil?
+#   end
+
+#   def peek
+#     return nil if self.empty?
+#     head.datum
+#   end
+
+#   def push(datum)
+#     # create a new Element object
+#     new_element = Element.new(datum, head)
+#     # change head to pushed
+#     self.head = new_element
+#   end
+
+#   def pop 
+#     # create temporary head element
+#     temp_head = head
+#     # set new head of list to the next element
+#     self.head = temp_head.next
+#     # return datum of old head
+#     temp_head.datum
+#   end
+
+#   def to_a
+#     array = []
+#     return array if self.empty?
+
+#     self.each do |element|
+#       array << element.datum
+#     end
+
+#     array
+#   end
+
+#   def reverse
+#     # create an array from SimpleLinkedList object
+#     temp_array = self.to_a
+#     # reverse order of data in array with Array#reverse
+#     temp_array.reverse!
+#     # transform reversed array into a NEW SimpleLinkedList
+#     SimpleLinkedList.from_a(temp_array)
+#   end
+  
+#   private
+
+#   # #each method to move through the linked from first Element to the last and return head Element - this enables use of Enumerable class methods such as #count
+
+#   def each
+#     current_element = head
+
+#     loop do
+#       yield(current_element)
+#       break if current_element.next.nil?
+#       current_element = current_element.next
+#     end
+
+#     head
+#   end
+# end
+
+# SOLUTION 2 - SECOND DRAFT - REFACTOR
 class Element
   attr_reader :datum
 
@@ -14,10 +122,6 @@ class Element
   def tail?
     self.next.nil?
   end
-
-  # def ==(other) # my own method for testing
-  #   self.datum == other.datum && self.next_element == other.next_element
-  # end
 end
 
 class SimpleLinkedList
@@ -27,7 +131,7 @@ class SimpleLinkedList
 
   def self.from_a(array)
     new_list = SimpleLinkedList.new
-    return new_list if array.nil?
+    return new_list if array.nil? || array.empty?
 
     array.reverse_each do |value|
       new_list.push(value)
@@ -41,7 +145,7 @@ class SimpleLinkedList
 
   def size
     return 0 if self.empty?
-    self.count # using Enumerable#count
+    self.count
   end
 
   def empty?
@@ -54,18 +158,13 @@ class SimpleLinkedList
   end
 
   def push(datum)
-    # create a new Element object
     new_element = Element.new(datum, head)
-    # change head to pushed
     self.head = new_element
   end
 
   def pop 
-    # create temporary head element
     temp_head = head
-    # set new head of list to the next element
     self.head = temp_head.next
-    # return datum of old head
     temp_head.datum
   end
 
@@ -81,17 +180,12 @@ class SimpleLinkedList
   end
 
   def reverse
-    # create an array from SimpleLinkedList object
     temp_array = self.to_a
-    # reverse order of data in array with Array#reverse
     temp_array.reverse!
-    # transform reversed array into a NEW SimpleLinkedList
     SimpleLinkedList.from_a(temp_array)
   end
   
   private
-
-  # #each method to move through the linked from first Element to the last and return head Element - this enables use of Enumerable class methods such as #count
 
   def each
     current_element = head
@@ -104,8 +198,6 @@ class SimpleLinkedList
 
     head
   end
-
-
 end
 
 # My Tests
@@ -117,17 +209,17 @@ end
 # p element1.tail? == true
 # p element2.tail? == false
 
-test_list = SimpleLinkedList.new
+# test_list = SimpleLinkedList.new
 # p test_list.empty? == true
 # p test_list.size == 0
-test_list.push(1)
-test_list.push(2)
+# test_list.push(1)
+# test_list.push(2)
 # p test_list.peek #=> #<Element>
 # p test_list.head
 # test_list.each { |element| puts element.datum }
 # p test_list.count
 # p test_list.size == 2
 
-p test_list.to_a
+# p test_list.to_a
 # p SimpleLinkedList.new.to_a
-p test_list.reverse.to_a
+# p test_list.reverse.to_a
