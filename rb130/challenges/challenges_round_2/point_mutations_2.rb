055cc3b6459707b0b424bf1783b2df65f4b8cc1e
Graham Jarvis <zeroThousand111@proton.me>
2025-05-16 11:42:33 +0100

    point_mutations_2.rb and point_mutations_tests.rb challenges RB130

---
+++ rb130/challenges/challenges_round_2/point_mutations_2.rb
@@ -0,0 +1,53 @@
+# solution 1 - using a simple loop to iterate through index positions
+
+# class DNA
+#   attr_reader :string
+
+#   def initialize(string)
+#     @string = string
+#   end
+
+#   def hamming_distance(other_string)
+#     index = 0
+#     count = 0
+#     self_string = self.string
+#     self_length = self_string.length
+#     other_length = other_string.length
+
+#     loop do
+#       count += 1 if self_string[index] != other_string[index]
+#       index += 1
+#       break if index >= self_length || index >= other_length
+#     end
+
+#     count
+#   end
+# end
+
+# solution 2 - using Array#upto to iterate after determining max_length
+
+class DNA
+  attr_reader :string
+
+  def initialize(string)
+    @string = string
+  end
+
+  def hamming_distance(other_string)
+    count = 0
+
+    self_string = string
+    self_length = self_string.length
+    other_length = other_string.length
+
+    max_length = [self_length, other_length].min
+
+    0.upto(max_length) do |index|
+      count += 1 if self_string[index] != other_string[index]
+    end
+
+    count
+  end
+end
+
+p DNA.new('ACT').hamming_distance('TCA') # => 2
