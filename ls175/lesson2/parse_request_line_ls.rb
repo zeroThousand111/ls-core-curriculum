db99152378f86730967d638cf427ce30efdac424
Graham Jarvis <zeroThousand111@proton.me>
2025-08-13 11:26:46 +0100

    update parse_request_line_ls.rb to fix bug

--- ls175/lesson2/parse_request_line_ls.rb
+++ ls175/lesson2/parse_request_line_ls.rb
@@ -1,5 +1,18 @@
 require "socket"
 
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split(" ")
+
+  path, params = path_and_params.split("?")
+  
+  params = params.split("&").each_with_object({}) do |pair, hash|
+    key, value = pair.split("=")
+    hash[key] = value
+  end
+
+  [http_method, path, params]
+end
+
 server = TCPServer.new("localhost", 3003)
 
 loop do
@@ -8,18 +21,15 @@ loop do
   request_line = client.gets
   next if !request_line || request_line =~ /favicon/
 
-  http_method, path_and_params, http = request_line.split(" ")
+  puts request_line
 
-  path, params = path_and_params.split("?")
+  http_method, path, params = parse_request(request_line)
 
-  params = path_and_params.split("&").each_with_object({}) do |pair, hash|
-    key, value = pair.split(" ")
-    hash[key] = value
-  end
-
-  client.puts "HTTP/1.1 200 OK" # not in LS solution but required to work on modern browser versions
-  client.puts "Content-Type: text/plain\r\n\r\n" # ditto
+  client.puts "HTTP/1.1 200 OK"
+  client.puts "Content-Type: text/plain\r\n\r\n"
   client.puts request_line
+  client.puts path
+  client.puts params
 
   rolls = params["rolls"].to_i
   sides = params["sides"].to_i
